052b72dac12c14d07a95d9aa310e7ea265cd73a3
JJSosaL <hello@fancystudio.xyz>
2026-03-30 18:01:04 +0200

    chore: update index.ts

--- apps/client/src/index.ts
+++ apps/client/src/index.ts
@@ -1,4 +1,15 @@
 import { loadEnvFile } from 'node:process';
-import { getEnvFileName } from '#utils/getEnvFileName.js';
+import { bot } from './lib/Client.js';
+import { logger } from './lib/Logger.js';
+import { getEnvFileName } from './utils/getEnvFileName.js';
 
 loadEnvFile(getEnvFileName());
+
+bot.start();
+
+bot.events.debug = (information) => logger.debug(information);
+bot.events.ready = ({ user }) => {
+	const { username } = user;
+
+	logger.info(`Client ${username} has been connected`);
+};
