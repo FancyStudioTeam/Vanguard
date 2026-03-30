cc5984ee37b96f36cdf1a88b7a429b60c2f5de35
JJSosaL <hello@fancystudio.xyz>
2026-03-30 21:38:39 +0200

    chore: move events to bot instance

--- apps/client/src/index.ts
+++ apps/client/src/index.ts
@@ -1,13 +1,4 @@
 import './env.js';
-
 import { bot } from './lib/Client.js';
-import { logger } from './lib/Logger.js';
 
 bot.start();
-
-bot.events.debug = (information) => logger.debug(information);
-bot.events.ready = ({ user }) => {
-	const { username } = user;
-
-	logger.info(`Client ${username} has been connected`);
-};
--- apps/client/src/lib/Client.ts
+++ apps/client/src/lib/Client.ts
@@ -1,11 +1,24 @@
+import { platform } from 'node:process';
 import { createBot, GatewayIntents } from '@discordeno/bot';
 import { DISCORD_CLIENT } from './Constants.js';
+import { logger } from './Logger.js';
 
 export const bot = createBot({
 	desiredProperties: {
 		user: {
 			id: true,
 			username: true,
+		},
+	},
+	events: {
+		debug: (info) => logger.debug(info),
+		ready: ({ user: { username } }) => logger.info(`Client '${username}' has been connected`),
+	},
+	gateway: {
+		properties: {
+			browser: 'Discord Android',
+			device: 'Discordeno',
+			os: platform,
 		},
 	},
 	intents:
