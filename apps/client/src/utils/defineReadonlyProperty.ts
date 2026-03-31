2bce224ff075cea0f6e2128dd0ab0d748f909d60
JJSosaL <hello@fancystudio.xyz>
2026-03-31 22:57:05 +0200

    feat(utils): add `defineReadonlyProperty`

---
+++ apps/client/src/utils/defineReadonlyProperty.ts
@@ -0,0 +1,8 @@
+export function defineReadonlyProperty(object: object, name: string, value: unknown): void {
+	Reflect.defineProperty(object, name, {
+		configurable: false,
+		enumerable: false,
+		value,
+		writable: false,
+	});
+}
