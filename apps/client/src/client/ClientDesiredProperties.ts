95b37ab8a2352e18f7812fde4ee709dd7c9d7860
JJSosaL <hello@fancystudio.xyz>
2026-03-31 19:49:10 +0200

    chore: add more desired properties

--- apps/client/src/client/ClientDesiredProperties.ts
+++ apps/client/src/client/ClientDesiredProperties.ts
@@ -1,5 +1,13 @@
 import type { RecursivePartial, TransformersDesiredProperties } from '@discordeno/bot';
 
+export const DESIRED_GUILD_PROPERTIES: DesiredGuildProperties = {
+	icon: true,
+	id: true,
+	name: true,
+	ownerId: true,
+	toggles: true,
+};
+
 export const DESIRED_MESSAGE_PROPERTIES: DesiredMessageProperties = {
 	author: true,
 	channelId: true,
@@ -9,6 +17,7 @@ export const DESIRED_MESSAGE_PROPERTIES: DesiredMessageProperties = {
 };
 
 export const DESIRED_USER_PROPERTIES: DesiredUserProperties = {
+	avatar: true,
 	id: true,
 	toggles: true,
 	username: true,
@@ -16,5 +25,6 @@ export const DESIRED_USER_PROPERTIES: DesiredUserProperties = {
 
 export type DesiredProperties = RecursivePartial<TransformersDesiredProperties>;
 
+type DesiredGuildProperties = DesiredProperties['guild'];
 type DesiredMessageProperties = DesiredProperties['message'];
 type DesiredUserProperties = DesiredProperties['user'];
--- apps/client/src/client/ClientOptions.ts
+++ apps/client/src/client/ClientOptions.ts
@@ -1,12 +1,14 @@
 import { platform } from 'node:process';
 import type { CreateGatewayManagerOptions } from '@discordeno/bot';
 import {
+	DESIRED_GUILD_PROPERTIES,
 	DESIRED_MESSAGE_PROPERTIES,
 	DESIRED_USER_PROPERTIES,
 	type DesiredProperties,
 } from './ClientDesiredProperties.js';
 
 export const CLIENT_DESIRED_PROPERTIES: ClientDesiredProperties = {
+	guild: DESIRED_GUILD_PROPERTIES,
 	message: DESIRED_MESSAGE_PROPERTIES,
 	user: DESIRED_USER_PROPERTIES,
 };
