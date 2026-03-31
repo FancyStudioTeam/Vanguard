8cb823c482a223bf5d36da6240c754f116019896
JJSosaL <hello@fancystudio.xyz>
2026-03-31 22:58:24 +0200

    chore: add more properties to bot

--- apps/client/src/bot/Bot.ts
+++ apps/client/src/bot/Bot.ts
@@ -1,7 +1,9 @@
 import { createBot } from '@discordeno/bot';
+import { Collection } from '@discordjs/collection';
 import { CommandManager } from '#handlers/commands/CommandManager.js';
 import { EventManager } from '#handlers/events/EventManager.js';
 import { DISCORD_GATEWAY_INTENTS, DISCORD_TOKEN } from '#lib/Constants.js';
+import { defineReadonlyProperty } from '#utils/defineReadonlyProperty.js';
 import { BOT_DESIRED_PROPERTIES, BOT_GATEWAY_MANAGER_PROPERTIES } from './BotOptions.js';
 import type { Bot } from './BotTypes.js';
 
@@ -16,19 +18,11 @@ export const discordenoBot = createBot({
 });
 export const bot = discordenoBot as Bot;
 
-Object.defineProperties(bot, {
-	commandManager: {
-		configurable: false,
-		enumerable: false,
-		value: new CommandManager(bot),
-		writable: false,
-	},
-	eventManager: {
-		configurable: false,
-		enumerable: false,
-		value: new EventManager(bot),
-		writable: false,
-	},
+defineReadonlyProperty(bot, 'commandManager', new CommandManager(bot));
+defineReadonlyProperty(bot, 'eventManager', new EventManager(bot));
+
+defineReadonlyProperty(bot, 'commands', {
+	chatInput: new Collection(),
 });
 
 await Promise.allSettled([
--- apps/client/src/bot/BotTypes.ts
+++ apps/client/src/bot/BotTypes.ts
@@ -1,9 +1,14 @@
+import type { Collection } from '@discordjs/collection';
 import type { CommandManager } from '#handlers/commands/CommandManager.js';
+import type { ChatInputCommandHandler } from '#handlers/commands/structures/ChatInputCommandHandler.js';
 import type { EventManager } from '#handlers/events/EventManager.js';
 import type { discordenoBot } from './Bot.js';
 
 export type Bot = typeof discordenoBot & {
 	readonly commandManager: CommandManager;
+	readonly commands: {
+		readonly chatInput: Collection<string, ChatInputCommandHandler>;
+	};
 	readonly eventManager: EventManager;
 };
 
