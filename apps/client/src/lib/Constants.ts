1191a30ea26efb0158a25b1bbdcf767e0ce48e19
JJSosaL <hello@fancystudio.xyz>
2026-04-02 01:41:58 +0200

    chore: delete unnecessary functions

--- apps/client/src/lib/Constants.ts
+++ apps/client/src/lib/Constants.ts
@@ -1,10 +1,6 @@
 import { GatewayIntents } from '@discordeno/bot';
 import { getEnvVariable } from '#utils/getEnvVariable.js';
-import { reduceGatewayIntents } from '#utils/reduceGatewayIntents.js';
 
-export const DISCORD_GATEWAY_INTENTS = reduceGatewayIntents(
-	GatewayIntents.GuildMembers,
-	GatewayIntents.Guilds,
-	GatewayIntents.MessageContent,
-);
+export const DISCORD_GATEWAY_INTENTS =
+	GatewayIntents.GuildMembers | GatewayIntents.Guilds | GatewayIntents.MessageContent;
 export const DISCORD_TOKEN = getEnvVariable('DISCORD_TOKEN');
--- apps/client/src/utils/reduceGatewayIntents.ts
+++
@@ -1,5 +0,0 @@
-import type { GatewayIntents } from '@discordeno/bot';
-
-export function reduceGatewayIntents(...gatewayIntents: GatewayIntents[]): GatewayIntents {
-	return Number(gatewayIntents.reduce((previous, intent) => previous | intent, 0));
-}
