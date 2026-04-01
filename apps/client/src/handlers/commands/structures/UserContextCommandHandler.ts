42339047d081058dee577d768dca6b1a0b80d7e8
JJSosaL <hello@fancystudio.xyz>
2026-04-01 22:11:51 +0200

    feat: add `getTargetUser` in `UserContextCommandHandler`

--- apps/client/src/handlers/commands/structures/UserContextCommandHandler.ts
+++ apps/client/src/handlers/commands/structures/UserContextCommandHandler.ts
@@ -1,11 +1,35 @@
 import { ApplicationCommandTypes, type CreateApplicationCommand } from '@discordeno/bot';
 import { HandlerBase } from '#handlers/base/HandlerBase.js';
+import type { User } from '#lib/InferredTypes.js';
 import type { Constructor } from '#lib/Types.js';
 
 export abstract class UserContextCommandHandler extends HandlerBase {
 	public declare readonly declare: UserContextCommandHandlerDeclareOptions;
 
 	public abstract run(): unknown;
+
+	public getTargetUser(): User {
+		const { data } = this.getInteraction();
+
+		if (!data) {
+			throw new TypeError('Cannot get target user without data');
+		}
+
+		const { resolved, targetId } = data;
+		const { users } = resolved ?? {};
+
+		if (!targetId) {
+			throw new TypeError('Cannot get target user without target ID');
+		}
+
+		const targetUser = users?.get(targetId);
+
+		if (!targetUser) {
+			throw new TypeError('Target user is always present on user context commands');
+		}
+
+		return targetUser;
+	}
 
 	public toOptions(): CreateApplicationCommand {
 		const { declare } = this;
