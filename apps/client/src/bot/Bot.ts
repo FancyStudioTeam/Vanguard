834531c8163e4e1c9e525f577b9fd585bb0f22e8
JJSosaL <hello@fancystudio.xyz>
2026-04-02 21:13:44 +0200

    chore: use ddenox for handling

--- apps/client/src/bot/Bot.ts
+++ apps/client/src/bot/Bot.ts
@@ -1,7 +1,10 @@
-import { Collection, createBot } from '@discordeno/bot';
-import { CommandManager } from '#handlers/commands/CommandManager.js';
-import { EventManager } from '#handlers/events/EventManager.js';
-import { defineReadonlyProperty } from '#utils/defineReadonlyProperty.js';
+import { createBot } from '@discordeno/bot';
+import {
+	createEnhancedBot,
+	type ParseDesiredProperties,
+	type ParseDesiredPropertiesBehavior,
+} from 'ddenox';
+import { isProductionEnvironment } from '#utils/isProductionEnvironment.js';
 import {
 	BOT_DESIRED_PROPERTIES,
 	BOT_GATEWAY_MANAGER,
@@ -16,22 +19,22 @@ export const discordenoBot = createBot({
 	intents: BOT_INTENTS,
 	token: BOT_TOKEN,
 });
-export const bot = discordenoBot as Bot;
-
-defineReadonlyProperty(bot, 'commandManager', new CommandManager(bot));
-defineReadonlyProperty(bot, 'eventManager', new EventManager(bot));
-
-defineReadonlyProperty(bot, 'commands', {
-	chatInput: new Collection(),
-	messageContext: new Collection(),
-	userContext: new Collection(),
+export const enhancedBot = createEnhancedBot(discordenoBot, {
+	locations: {
+		commands: 'commands',
+		events: 'events',
+		root: isProductionEnvironment() ? 'dist' : 'src',
+	},
 });
 
-const { commandManager, eventManager } = bot;
+export const bot = enhancedBot as Bot;
 
-Promise.all([
-	commandManager.initialize(),
-	eventManager.initialize(),
-]);
-
+bot.initializeModules();
 bot.start();
+
+declare module 'ddenox' {
+	interface BotConfiguration {
+		desiredProperties: ParseDesiredProperties<Bot>;
+		desiredPropertiesBehavior: ParseDesiredPropertiesBehavior<Bot>;
+	}
+}
--- apps/client/src/bot/BotTypes.ts
+++ apps/client/src/bot/BotTypes.ts
@@ -1,21 +1,6 @@
-import type { Collection } from '@discordeno/bot';
-import type { CommandManager } from '#handlers/commands/CommandManager.js';
-import type { ChatInputCommandHandler } from '#handlers/commands/structures/ChatInputCommandHandler.js';
-import type { UserContextCommandHandler } from '#handlers/commands/structures/UserContextCommandHandler.js';
-import type { EventManager } from '#handlers/events/EventManager.js';
-import type { discordenoBot } from './Bot.js';
+import type { enhancedBot } from './Bot.js';
 
-export type Bot = typeof discordenoBot & {
-	readonly commandManager: CommandManager;
-	readonly commands: {
-		readonly chatInput: Collection<string, ChatInputCommandHandler>;
-		readonly userContext: Collection<string, UserContextCommandHandler>;
-	};
-	readonly eventManager: EventManager;
-};
-
-export type BotEventNames = keyof BotEvents;
-export type BotEvents = Bot['events'];
+export type Bot = typeof enhancedBot;
 
 export type BotInferredTypes = BotTransformers['$inferredTypes'];
 
