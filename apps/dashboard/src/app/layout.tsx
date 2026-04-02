d4f6d0d32ec993aaf123d52974a86c16b27382bb
JJSosaL <hello@fancystudio.xyz>
2026-04-03 01:33:09 +0200

    chore: things

--- apps/dashboard/src/app/layout.tsx
+++ apps/dashboard/src/app/layout.tsx
@@ -5,6 +5,7 @@ import gsap from 'gsap';
 import { SplitText } from 'gsap/SplitText';
 import type { Metadata } from 'next';
 import type { ReactNode } from 'react';
+import { Navbar } from '#/components/navbar/Navbar.tsx';
 import { GeneralSansVariable } from '#/lib/Fonts.ts';
 import { MetadataPage } from '#/lib/Metadata.ts';
 import { createMetadataObject } from '#/utils/createMetadataObject.ts';
@@ -16,7 +17,12 @@ export const metadata: Metadata = createMetadataObject(MetadataPage.Home);
 export default function ({ children }: { children: ReactNode }) {
 	return (
 		<html lang='en-US'>
-			<body className={`${GeneralSansVariable} size-full font-general-sans`}>{children}</body>
+			<body
+				className={`${GeneralSansVariable} mx-auto h-dvh w-full max-w-7xl bg-dark-950 bg-radial from-dark-900 to-dark-950 py-6 font-general-sans font-medium text-dark-50`}
+			>
+				<Navbar />
+				{children}
+			</body>
 		</html>
 	);
 }
