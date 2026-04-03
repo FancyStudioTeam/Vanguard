816c37ec21fe04f23e6e739c1b296ea487d4a06d
JJSosaL <hello@fancystudio.xyz>
2026-04-03 18:38:57 +0200

    chore: update styles

--- apps/dashboard/src/app/layout.tsx
+++ apps/dashboard/src/app/layout.tsx
@@ -5,7 +5,6 @@ import gsap from 'gsap';
 import { SplitText } from 'gsap/SplitText';
 import type { Metadata } from 'next';
 import type { ReactNode } from 'react';
-import { Navbar } from '#/components/navbar/Navbar.tsx';
 import { GeneralSansVariable } from '#/lib/Fonts.ts';
 import { MetadataPage } from '#/lib/Metadata.ts';
 import { createMetadataObject } from '#/utils/createMetadataObject.ts';
@@ -18,9 +17,8 @@ export default function ({ children }: { children: ReactNode }) {
 	return (
 		<html lang='en-US'>
 			<body
-				className={`${GeneralSansVariable} mx-auto h-dvh w-full max-w-7xl bg-dark-950 bg-radial from-dark-900 to-dark-950 py-6 font-general-sans font-medium text-dark-50`}
+				className={`${GeneralSansVariable} mx-auto w-full max-w-7xl bg-neutral-950 font-general-sans font-medium text-zinc-50`}
 			>
-				<Navbar />
 				{children}
 			</body>
 		</html>
