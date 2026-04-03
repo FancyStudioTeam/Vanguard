b7db4b0e2bd33575507f2de28445e7ab086eb559
JJSosaL <hello@fancystudio.xyz>
2026-04-03 19:40:14 +0200

    refactor: Navbar component

--- apps/dashboard/src/components/navbar/Navbar.tsx
+++ apps/dashboard/src/components/navbar/Navbar.tsx
@@ -1,18 +1,11 @@
-import { VanguardCombinationMark } from '../shared/branding/VanguardCombinationMark.tsx';
 import { NavbarCtaButton } from './NavbarCtaButton.tsx';
+import { NavbarLogo } from './NavbarLogo.tsx';
 
 export function Navbar() {
 	return (
-		<nav className='sticky top-0 z-10 flex h-10 items-center divide-x border-b bg-neutral-950/75 backdrop-blur-xl'>
-			<div className='flex size-full items-center gap-4'>
-				<VanguardCombinationMark
-					className='mx-8'
-					href='/'
-				/>
-			</div>
-			<div className='flex size-full max-w-50'>
-				<NavbarCtaButton />
-			</div>
+		<nav className='sticky top-6 flex items-center justify-between rounded-xl border-2 border-neutral-800 bg-neutral-900 px-6 py-2'>
+			<NavbarLogo />
+			<NavbarCtaButton />
 		</nav>
 	);
 }
