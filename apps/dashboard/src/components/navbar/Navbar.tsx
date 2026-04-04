6f8bc34d83f4e8218bf54c84a8c715033184b151
JJSosaL <hello@fancystudio.xyz>
2026-04-04 21:17:11 +0200

    chore: add `Vanguard.avif` and add `z-50` to navbar

--- apps/dashboard/src/components/navbar/Navbar.tsx
+++ apps/dashboard/src/components/navbar/Navbar.tsx
@@ -3,7 +3,7 @@ import { NavbarLogo } from './NavbarLogo.tsx';
 
 export function Navbar() {
 	return (
-		<nav className='sticky top-6 flex items-center justify-between rounded-xl border-2 border-neutral-800 bg-neutral-900 px-6 py-2'>
+		<nav className='sticky top-6 z-50 flex items-center justify-between rounded-xl border-2 border-neutral-800 bg-neutral-900 px-6 py-2'>
 			<NavbarLogo />
 			<NavbarCtaButton />
 		</nav>
