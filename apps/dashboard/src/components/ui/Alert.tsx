2c1199241e1e26042d9117144c4f1efb0d6581df
JJSosaL <hello@fancystudio.xyz>
2026-04-04 20:40:50 +0200

    feat(ui): add `AlertDescription` component

--- apps/dashboard/src/components/ui/Alert.tsx
+++ apps/dashboard/src/components/ui/Alert.tsx
@@ -1,12 +1,15 @@
 import { cva, type VariantProps } from 'class-variance-authority';
 import type { ComponentProps } from 'react';
+import { twMerge } from 'tailwind-merge';
 
 const alertVariants = cva(
-	'flex items-center gap-2 rounded-xl border-2 border-neutral-800 bg-neutral-900 p-4 text-sm',
+	'flex items-center gap-2 rounded-xl border-2 border-neutral-800 bg-neutral-900 p-4',
 	{
 		variants: {
 			variant: {
 				amber: 'text-amber-400',
+				cyan: 'text-cyan-400',
+				default: 'text-neutral-400',
 				emerald: 'text-emerald-400',
 				rose: 'text-rose-400',
 			},
@@ -14,7 +17,7 @@ const alertVariants = cva(
 	},
 );
 
-export function Alert({ className, variant, ...props }: AlertProps) {
+export function Alert({ className, variant = 'default', ...props }: AlertProps) {
 	return (
 		<div
 			className={alertVariants({
@@ -26,4 +29,14 @@ export function Alert({ className, variant, ...props }: AlertProps) {
 	);
 }
 
+export function AlertDescription({ className, ...props }: AlertDescriptionProps) {
+	return (
+		<span
+			className={twMerge('text-balance text-sm', className)}
+			{...props}
+		/>
+	);
+}
+
+export type AlertDescriptionProps = ComponentProps<'span'>;
 export type AlertProps = ComponentProps<'div'> & VariantProps<typeof alertVariants>;
