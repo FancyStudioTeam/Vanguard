b370123a3eb0aae712a679c587c4faff715e63db
JJSosaL <hello@fancystudio.xyz>
2026-04-06 01:04:20 +0200

    refactor(createRedirectUrl): use `createOAuth2Link` instead of manual

--- apps/dashboard/src/utils/URL/createRedirectUrl.ts
+++ apps/dashboard/src/utils/URL/createRedirectUrl.ts
@@ -1,38 +1,22 @@
 import { OAuth2Scope } from '@discordeno/types';
+import { createOAuth2Link } from '@discordeno/utils';
 import { CLIENT_ID } from '#lib/Constants/Client.ts';
 import { createCallbackUrl } from './createCallbackUrl.ts';
 
-const SCOPES = [
-	OAuth2Scope.Email,
-	OAuth2Scope.Identify,
-	OAuth2Scope.Guilds,
-	OAuth2Scope.GuildsMembersRead,
-];
-
 export function createRedirectUrl(): string {
 	const callbackUrl = createCallbackUrl();
+	const redirectUrl = createOAuth2Link({
+		clientId: CLIENT_ID,
+		prompt: 'none',
+		redirectUri: callbackUrl,
+		responseType: 'code',
+		scope: [
+			OAuth2Scope.Email,
+			OAuth2Scope.Identify,
+			OAuth2Scope.Guilds,
+			OAuth2Scope.GuildsMembersRead,
+		],
+	});
 
-	/*
-	 * Discord scopes MUST be joined with '+'.
-	 */
-	const scopesString = SCOPES.join('+');
-
-	const authorizationUrl = new URL(
-		'https://discord.com/api/v10/oauth2/authorize',
-	);
-	const { searchParams } = authorizationUrl;
-
-	/*
-	 * This assignment MUST be made before setting other query string parameters,
-	 * as this property overrides all search properties.
-	 */
-	authorizationUrl.search = `scope=${scopesString}`;
-
-	searchParams.set('client_id', CLIENT_ID);
-	searchParams.set('redirect_uri', callbackUrl);
-	searchParams.set('response_type', 'code');
-
-	searchParams.set('prompt', 'none');
-
-	return authorizationUrl.toString();
+	return redirectUrl;
 }
