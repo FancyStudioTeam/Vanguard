a087ec4f8f9f9d6c5b0b9a053a093771c2083eee
JJSosaL <hello@fancystudio.xyz>
2026-04-05 19:40:02 +0200

    chore: things

---
+++ apps/client/src/events/interactionCreate.event.ts
@@ -0,0 +1,38 @@
+import { InteractionTypes } from '@discordeno/bot';
+import { defineEventListener } from 'ddenox/handlers';
+import { bot } from '#bot/Bot.js';
+
+export default defineEventListener({
+	data: {
+		name: 'interactionCreate',
+	},
+	run(interaction) {
+		const { data, type } = interaction;
+
+		if (!data) {
+			return;
+		}
+
+		switch (type) {
+			case InteractionTypes.ApplicationCommand: {
+				const { commandManager } = bot;
+				const { commands } = commandManager;
+
+				const { type, name } = data;
+
+				const commandKey = `${type}.${name}`;
+
+				const command = commands.get(commandKey);
+
+				if (command) {
+					command['setBot'](bot);
+					command['setInteraction'](interaction);
+
+					command.run();
+				}
+
+				break;
+			}
+		}
+	},
+});
