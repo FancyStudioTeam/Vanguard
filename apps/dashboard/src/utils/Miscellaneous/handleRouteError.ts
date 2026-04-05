f53c53bd2f7a549028420ae6de6bf33637a49ebe
JJSosaL <hello@fancystudio.xyz>
2026-04-05 19:08:16 +0200

    chore: use predefined function

--- apps/dashboard/src/utils/Miscellaneous/handleRouteError.ts
+++ apps/dashboard/src/utils/Miscellaneous/handleRouteError.ts
@@ -1,23 +1,15 @@
 import type { NextRequest, NextResponse } from 'next/server';
-import {
-	INTERNAL_SERVER_ERROR_STATUS_CODE,
-	INTERNAL_SERVER_ERROR_STATUS_TEXT,
-} from '#lib/HttpStatus.ts';
 import { logger } from '#lib/Logger.ts';
-import { createErrorJsonResponse } from '#utils/Responses/createErrorJsonResponse.ts';
+import { INTERNAL_SERVER_ERROR_RESPONSE } from '#lib/Responses/Shared.ts';
 
-export function handleRouteError(nextRequest: NextRequest, error: unknown): NextResponse {
+export function handleRouteError(
+	nextRequest: NextRequest,
+	error: unknown,
+): NextResponse {
 	const { nextUrl } = nextRequest;
 	const { href } = nextUrl;
 
 	logger.error(`Error while processing API route '${href}':\n\t`, error);
 
-	return createErrorJsonResponse(
-		INTERNAL_SERVER_ERROR_STATUS_CODE,
-		INTERNAL_SERVER_ERROR_STATUS_TEXT,
-		{
-			code: 'INTERNAL_SERVER_ERROR',
-			message: 'Something went wrong while processing your request',
-		},
-	);
+	return INTERNAL_SERVER_ERROR_RESPONSE();
 }
