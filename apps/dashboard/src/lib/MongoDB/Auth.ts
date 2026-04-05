4f88a9c18eb9b2c81f88a1642d7d60b9a0c4a2e4
JJSosaL <hello@fancystudio.xyz>
2026-04-05 20:57:15 +0200

    refactor: mongodb

---
+++ apps/dashboard/src/lib/MongoDB/Auth.ts
@@ -0,0 +1,44 @@
+import { type Collection, type Document, MongoClient } from 'mongodb';
+import {
+	MONGO_DB_COLLECTION_NAME,
+	MONGO_DB_CONNECTION_URL,
+	MONGO_DB_DATABASE_NAME,
+	NODE_ENV,
+} from '#lib/Constants.ts';
+
+export const client =
+	global.mongoClient ?? new MongoClient(MONGO_DB_CONNECTION_URL);
+
+if (NODE_ENV === 'development') {
+	global.mongoClient = client;
+}
+
+const clientPromise: Promise<MongoClient> = client.connect();
+
+export async function getCollection<CollectionDefinition extends Document>(
+	collectionName: string,
+): Promise<Collection<CollectionDefinition>> {
+	const client = await clientPromise;
+	const db = client.db(MONGO_DB_DATABASE_NAME);
+	const collection = db.collection<CollectionDefinition>(collectionName);
+
+	return collection;
+}
+
+export const SessionsCollection = await getCollection<SessionDocument>(
+	MONGO_DB_COLLECTION_NAME,
+);
+
+export interface SessionDocument {
+	credentials: SessionDocumentCredentials;
+	sessionId: string;
+}
+
+export interface SessionDocumentCredentials {
+	accessToken: string;
+	refreshToken: string;
+}
+
+declare global {
+	var mongoClient: MongoClient | null;
+}
