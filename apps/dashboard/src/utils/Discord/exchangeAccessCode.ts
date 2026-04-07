28ad3ad02638e49ce989c197e3e1c252757a2fa4
JJSosaL <hello@fancystudio.xyz>
2026-04-07 22:54:53 +0200

    chore: things

--- apps/dashboard/src/utils/Discord/exchangeAccessCode.ts
+++ apps/dashboard/src/utils/Discord/exchangeAccessCode.ts
@@ -1,11 +1,11 @@
+import type { Camelize, DiscordAccessTokenResponse } from '@discordeno/types';
 import { CLIENT_ID, CLIENT_SECRET } from '#lib/Constants/Client.ts';
 import { rest } from '#lib/REST.ts';
-import type { UserCredentials } from '#types/Discord.ts';
 import { createCallbackUrl } from '#utils/URL/createCallbackUrl.ts';
 
 export async function exchangeAccessCode(
 	code: string,
-): Promise<UserCredentials> {
+): Promise<Camelize<DiscordAccessTokenResponse>> {
 	return await rest.exchangeToken(CLIENT_ID, CLIENT_SECRET, {
 		code,
 		grantType: 'authorization_code',
