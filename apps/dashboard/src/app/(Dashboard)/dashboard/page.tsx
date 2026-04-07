fcac0814d0413c3870230e8f81d701127d3fb954
JJSosaL <hello@fancystudio.xyz>
2026-04-07 23:29:25 +0200

    chore: changes

--- apps/dashboard/src/app/(Dashboard)/dashboard/page.tsx
+++ apps/dashboard/src/app/(Dashboard)/dashboard/page.tsx
@@ -1,21 +1,18 @@
 import { Suspense } from 'react';
 import { GuildSelector } from '#components/dashboard/guilds/GuildSelector.tsx';
 import { GuildSelectorFallback } from '#components/dashboard/guilds/GuildSelectorFallback.tsx';
-import { PageLayout } from '#layouts/PageLayout.tsx';
 import { verifySession } from '#utils/Session/verifySession.ts';
 
 export default async function () {
-	const { accessToken, user } = await verifySession(true);
-	const { name } = user;
+	const { credentials } = await verifySession();
+	const { accessToken } = credentials;
 
 	return (
-		<PageLayout>
-			<h1 className='text-center font-bold text-2xl'>
-				Welcome back, <span className='text-neutral-400'>{name}</span>!
-			</h1>
+		<>
+			<h1 className='text-center font-bold text-2xl'>Welcome back!</h1>
 			<Suspense fallback={<GuildSelectorFallback />}>
 				<GuildSelector accessToken={accessToken} />
 			</Suspense>
-		</PageLayout>
+		</>
 	);
 }
--- apps/dashboard/src/utils/Session/verifySession.ts
+++ apps/dashboard/src/utils/Session/verifySession.ts
@@ -1,59 +1,31 @@
 import { unauthorized } from 'next/navigation';
-import { SessionsCollection } from '#lib/MongoDB/Auth.ts';
-import type { AuthJsonWebTokenPayload } from '#types/Auth.ts';
+import { type SessionDocument, SessionsCollection } from '#lib/MongoDB/Auth.ts';
 import { decryptData } from '#utils/Jose/decryptData.ts';
-import { verifyJsonWebToken } from '#utils/Jose/verifyJsonWebToken.ts';
-import { getSessionCookieValue } from './getSessionId.ts';
+import { getSessionId } from './getSessionId.ts';
 
-export async function verifySession(
-	withCredentials?: false,
-): Promise<SessionWithoutCredentials>;
-export async function verifySession(
-	withCredentials: true,
-): Promise<SessionWithCredentials>;
+export async function verifySession(): Promise<
+	Omit<SessionDocument, 'sessionId'>
+> {
+	const sessionId = await getSessionId();
+	const session = await SessionsCollection.findOne({
+		sessionId,
+	});
 
-export async function verifySession(
-	withCredentials?: boolean,
-): Promise<AuthJsonWebTokenPayload> {
-	const sessionCookieValue = await getSessionCookieValue();
-	const jsonWebTokenPayload = await verifyJsonWebToken(
-		sessionCookieValue ?? '',
-	);
-
-	if (!jsonWebTokenPayload) {
+	if (!session) {
 		unauthorized();
 	}
 
-	if (!withCredentials) {
-		return jsonWebTokenPayload;
-	} else {
-		const { sid: sessionId } = jsonWebTokenPayload;
+	const { credentials, user } = session;
+	const { accessToken, refreshToken } = credentials;
 
-		const sessionData = await SessionsCollection.findOne({
-			sessionId,
-		});
+	const decryptedAccessToken = await decryptData(accessToken);
+	const decryptedRefreshToken = await decryptData(refreshToken);
 
-		if (!sessionData) {
-			unauthorized();
-		}
-
-		const { credentials } = sessionData;
-		const { accessToken, refreshToken } = credentials;
-
-		const unencryptedAccessToken = await decryptData(accessToken);
-		const unencryptedRefreshToken = await decryptData(refreshToken);
-
-		return {
-			...jsonWebTokenPayload,
-			accessToken: unencryptedAccessToken,
-			refreshToken: unencryptedRefreshToken,
-		};
-	}
+	return {
+		credentials: {
+			accessToken: decryptedAccessToken,
+			refreshToken: decryptedRefreshToken,
+		},
+		user,
+	};
 }
-
-interface SessionWithCredentials extends AuthJsonWebTokenPayload {
-	accessToken: string;
-	refreshToken: string;
-}
-
-type SessionWithoutCredentials = AuthJsonWebTokenPayload;
