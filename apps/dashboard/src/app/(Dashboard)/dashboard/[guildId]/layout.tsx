dce88d9c283faeef210b091cc36f32f0b261818c
JJSosaL <hello@fancystudio.xyz>
2026-04-08 00:00:58 +0200

    chore: update dashboard layout

--- apps/dashboard/src/app/(Dashboard)/dashboard/[guildId]/layout.tsx
+++ apps/dashboard/src/app/(Dashboard)/dashboard/[guildId]/layout.tsx
@@ -1,20 +1,34 @@
+import type { ReactNode } from 'react';
 import { Sidebar } from '#components/dashboard/sidebar/Sidebar.tsx';
+import { verifyGuild } from '#utils/Session/verifyGuild.ts';
+import { verifySession } from '#utils/Session/verifySession.ts';
 
-export default async function ({
-	params,
-}: {
-	params: Promise<{
-		guildId: string;
-	}>;
-}) {
+export default async function ({ children, params }: LayoutProps) {
 	const { guildId } = await params;
 
+	const { credentials, user } = await verifySession();
+
+	const { accessToken } = credentials;
+	const { id } = user;
+
+	const guild = await verifyGuild(guildId, {
+		accessToken,
+		userId: id,
+	});
+
 	return (
-		<div className='h-dvh w-full'>
-			<Sidebar guildId={guildId} />
-			<main className='lg:ml-100'>
-				<h1>Hola</h1>
-			</main>
+		<div className='flex flex-row gap-6'>
+			<Sidebar guild={guild} />
+			<main className='flex w-full flex-col gap-6'>{children}</main>
 		</div>
 	);
+}
+
+interface LayoutProps {
+	children: ReactNode;
+	params: Promise<LayoutPropsParams>;
+}
+
+interface LayoutPropsParams {
+	guildId: string;
 }
