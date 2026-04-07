5463ba52f593ecd9e874f62f338bce0d2a25bd27
JJSosaL <hello@fancystudio.xyz>
2026-04-07 20:44:58 +0200

    chore: little changes

--- apps/dashboard/src/components/dashboard/sidebar/header/SidebarHeaderAvatar.tsx
+++ apps/dashboard/src/components/dashboard/sidebar/header/SidebarHeaderAvatar.tsx
@@ -1,17 +1,16 @@
+import { guildIconUrl } from '@discordeno/utils';
 import { ImageIcon } from '@phosphor-icons/react/dist/ssr';
 import { Avatar, AvatarFallback, AvatarImage } from '#components/ui/Avatar.tsx';
 import type { SidebarHeaderProps } from './SidebarHeader.tsx';
 
 export function SidebarHeaderAvatar({ icon, id }: SidebarHeaderAvatarProps) {
-	const guildIconUrl = `https://cdn.discordapp.com/icons/${id}/${icon}.webp?size=512`;
-
 	return (
 		<Avatar>
 			<AvatarImage
 				className='size-10 bg-neutral-800'
-				src={guildIconUrl}
+				src={guildIconUrl(id, icon ?? undefined)}
 			/>
-			<AvatarFallback className='size-10'>
+			<AvatarFallback className='size-10 bg-neutral-900'>
 				<ImageIcon
 					className='size-5 shrink-0'
 					weight='fill'
