2068008e1f3538a69a42256bbbfbf4e31cee7221
JJSosaL <hello@fancystudio.xyz>
2026-04-10 00:09:39 +0200

    fix: add auth tag

--- apps/vanguard-api/src/services/EncryptionService.ts
+++ apps/vanguard-api/src/services/EncryptionService.ts
@@ -4,8 +4,8 @@ import { getEnvVariable } from '#utils/Process/getEnvVariable.js';
 
 @Injectable()
 export class EncryptionService {
-	static ENCRYPTION_ALGORITHM = 'aes-256-gcm';
-	static ENCRYPTION_IV_LENGTH = 16;
+	static ENCRYPTION_ALGORITHM = 'aes-256-gcm' as const;
+	static ENCRYPTION_IV_LENGTH = 16 as const;
 	static ENCRYPTION_SECRET = getEnvVariable('ENCRYPTION_SECRET');
 
 	public encrypt(plainData: string): string {
@@ -14,6 +14,8 @@ export class EncryptionService {
 		const ivBytes = randomBytes(ENCRYPTION_IV_LENGTH);
 		const cipher = createCipheriv(ENCRYPTION_ALGORITHM, ENCRYPTION_SECRET, ivBytes);
 
+		const authTag = cipher.getAuthTag();
+
 		const encryptedBuffer = Buffer.concat([
 			cipher.update(plainData, 'utf-8'),
 			cipher.final(),
@@ -21,6 +23,7 @@ export class EncryptionService {
 
 		const result = [
 			ivBytes,
+			authTag,
 			encryptedBuffer,
 		]
 			.map((buffer) => buffer.toString('hex'))
