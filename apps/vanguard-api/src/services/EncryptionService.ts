d64ff12a2dd87c884d0cd390b748f7477428cb7e
J.J. Sosa L. <251519186+jjsosal@users.noreply.github.com>
2026-04-10 00:50:14 +0200

    chore: things

--- apps/vanguard-api/src/services/EncryptionService.ts
+++ apps/vanguard-api/src/services/EncryptionService.ts
@@ -1,4 +1,4 @@
-import { createCipheriv, randomBytes } from 'node:crypto';
+import { createCipheriv, createDecipheriv, randomBytes } from 'node:crypto';
 import { Injectable } from '@nestjs/common';
 import { getEnvVariable } from '#utils/Process/getEnvVariable.js';
 
@@ -7,6 +7,29 @@ export class EncryptionService {
 	static ENCRYPTION_ALGORITHM = 'aes-256-gcm' as const;
 	static ENCRYPTION_IV_LENGTH = 16 as const;
 	static ENCRYPTION_SECRET = getEnvVariable('ENCRYPTION_SECRET');
+
+	public decrypt(encryptedData: string): string {
+		const { ENCRYPTION_ALGORITHM, ENCRYPTION_SECRET } = EncryptionService;
+
+		const [ivHex, authTagHext, encryptedHex] = encryptedData.split(':');
+
+		const iv = Buffer.from(ivHex, 'hex');
+		const authTag = Buffer.from(authTagHext, 'hex');
+		const encrypted = Buffer.from(encryptedHex, 'hex');
+
+		const decipher = createDecipheriv(ENCRYPTION_ALGORITHM, ENCRYPTION_SECRET, iv);
+
+		decipher.setAuthTag(authTag);
+
+		const decryptedBuffer = Buffer.concat([
+			decipher.update(encrypted),
+			decipher.final(),
+		]);
+
+		const decryptedString = decryptedBuffer.toString('utf-8');
+
+		return decryptedString;
+	}
 
 	public encrypt(plainData: string): string {
 		const { ENCRYPTION_ALGORITHM, ENCRYPTION_IV_LENGTH, ENCRYPTION_SECRET } = EncryptionService;
