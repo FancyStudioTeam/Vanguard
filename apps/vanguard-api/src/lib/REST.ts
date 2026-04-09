3cdc379ab019139a2f0f19d81c1e702a1aa50060
JJSosaL <hello@fancystudio.xyz>
2026-04-09 16:40:07 +0200

    chore: things

---
+++ apps/vanguard-api/src/lib/REST.ts
@@ -0,0 +1,8 @@
+import { createRestManager } from '@discordeno/rest';
+import { CLIENT_TOKEN } from './Constants/Client.js';
+
+export const rest = createRestManager({
+	token: CLIENT_TOKEN,
+});
+
+rest.maxRetryCount = 0;
