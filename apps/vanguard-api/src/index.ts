70d71f8814fb5891d5f402117dbd87f7f1e6be74
JJSosaL <hello@fancystudio.xyz>
2026-04-09 19:26:32 +0200

    chore: select logger level errors

--- apps/vanguard-api/src/index.ts
+++ apps/vanguard-api/src/index.ts
@@ -7,14 +7,17 @@ import {
 	FastifyAdapter,
 	type NestFastifyApplication,
 } from '@nestjs/platform-fastify';
-import { AppModule } from '#modules/app.module.js';
+import { AppModule } from '#modules/App.module.js';
 
 const PORT = 3001;
 
 const APP_MODULE = AppModule;
 const APP_ADAPTER = new FastifyAdapter();
 const APP_OPTIONS: NestApplicationOptions = {
-	logger: false,
+	logger: [
+		'error',
+		'fatal',
+	],
 };
 
 const app = await NestFactory.create<NestFastifyApplication>(
