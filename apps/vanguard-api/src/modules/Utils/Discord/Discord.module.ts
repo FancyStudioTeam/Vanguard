0c735a08347d178a08a5baf4b8d4b8aeab0809c0
JJSosaL <hello@fancystudio.xyz>
2026-04-11 00:36:13 +0200

    fix: import `DiscordParserModule`

--- apps/vanguard-api/src/modules/Utils/Discord/Discord.module.ts
+++ apps/vanguard-api/src/modules/Utils/Discord/Discord.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { DiscordService } from './Discord.service.js';
+import { DiscordParserModule } from './Parser/DiscordParser.module.js';
 
 @Module({
 	exports: [
 		DiscordService,
+	],
+	imports: [
+		DiscordParserModule,
 	],
 	providers: [
 		DiscordService,
