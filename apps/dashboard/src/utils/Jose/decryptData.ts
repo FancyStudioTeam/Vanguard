27a5be17388f666cea8612cb98a10902a5c7d79a
JJSosaL <hello@fancystudio.xyz>
2026-04-10 20:33:12 +0200

    chore: things

--- apps/dashboard/src/utils/Jose/decryptData.ts
+++
@@ -1,8 +0,0 @@
-import { compactDecrypt } from 'jose';
-import { JOSE_ENCRYPTION_SECRET } from '#lib/Constants/Jose.ts';
-
-export async function decryptData(encryptedData: string): Promise<string> {
-	return await compactDecrypt(encryptedData, JOSE_ENCRYPTION_SECRET).then(
-		({ plaintext }) => new TextDecoder().decode(plaintext),
-	);
-}
