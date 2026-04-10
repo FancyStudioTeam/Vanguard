4a77b95807ed1c8f04738e4ca7b0194e22717048
JJSosaL <hello@fancystudio.xyz>
2026-04-10 22:53:35 +0200

    chore: add comment

--- apps/dashboard/src/lib/Constants/Shared.ts
+++ apps/dashboard/src/lib/Constants/Shared.ts
@@ -1,6 +1,7 @@
 import { env } from 'node:process';
 import { getEnvVariable } from '#utils/Process/getEnvVariable.ts';
 
+export const BASE_API_URL = getEnvVariable('BASE_API_URL');
 export const BASE_URL = getEnvVariable('BASE_URL');
 
 export const NODE_ENV = env.NODE_ENV;
