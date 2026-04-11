373c4e553673870df3b1e4c77076a8af35d10bda
JJSosaL <hello@fancystudio.xyz>
2026-04-11 17:01:10 +0200

    chore: things

--- apps/vanguard-api/src/lib/Constants/Shared.ts
+++ apps/vanguard-api/src/lib/Constants/Shared.ts
@@ -1,5 +1,8 @@
 import { getEnvVariable } from '#utils/Process/getEnvVariable.js';
 import { normalizeUrl } from '#utils/URL/normalizeUrl.js';
 
-export const BASE_DASHBOARD_URL = normalizeUrl(getEnvVariable('BASE_DASHBOARD_URL'));
-export const BASE_URL = normalizeUrl(getEnvVariable('BASE_URL'));
+const BASE_DASHBOARD_URL_VALUE = getEnvVariable('BASE_DASHBOARD_URL');
+const BASE_URL_VALUE = getEnvVariable('BASE_URL');
+
+export const BASE_DASHBOARD_URL = normalizeUrl(BASE_DASHBOARD_URL_VALUE);
+export const BASE_URL = normalizeUrl(BASE_URL_VALUE);
