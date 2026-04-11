122c5f1ffe0776a41e6dbfbec0dbf39d728f0376
JJSosaL <hello@fancystudio.xyz>
2026-04-11 23:02:21 +0200

    chore: add `Dockerfle` for `vanguard-client`

--- apps/vanguard-client/src/env.ts
+++ apps/vanguard-client/src/env.ts
@@ -1,6 +1,3 @@
 import { loadEnvFile } from 'node:process';
-import { getEnvFileName } from './utils/getEnvFileName.js';
 
-const envFileName = getEnvFileName();
-
-loadEnvFile(envFileName);
+loadEnvFile();
