c8160917cd4abd4e4cc16e44397924b7d90a3932
JJSosaL <hello@fancystudio.xyz>
2026-04-12 17:15:46 +0200

    fix: add missing `/auth`

--- apps/vanguard-api/src/utils/URL/createCallbackUrl.ts
+++ apps/vanguard-api/src/utils/URL/createCallbackUrl.ts
@@ -1,5 +1,5 @@
 import { BASE_URL } from '#lib/Constants/Shared.js';
 
 export function createCallbackUrl(): string {
-	return `${BASE_URL}/auth/callback`;
+	return `${BASE_URL}/api/auth/callback`;
 }
