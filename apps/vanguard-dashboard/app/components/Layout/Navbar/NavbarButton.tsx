4c9b15e9bcfbb4991c7dabcf6ef19b6c1fabb386
JJSosaL <hello@fancystudio.xyz>
2026-04-12 19:17:47 +0200

    feat(Navbar): add `NavbarButton` component

---
+++ apps/vanguard-dashboard/app/components/Layout/Navbar/NavbarButton.tsx
@@ -0,0 +1,22 @@
+import { GridFourIcon } from '@phosphor-icons/react';
+import { Link } from 'react-router';
+import { buttonVariants } from '#components/UI/Button.tsx';
+
+export function NavbarButton() {
+	return (
+		<Link
+			className={buttonVariants({
+				className: 'max-w-50',
+				variant: 'secondary',
+			})}
+			to={'/dashboard'}
+			viewTransition={true}
+		>
+			<GridFourIcon
+				className='size-5 shrink-0'
+				weight='fill'
+			/>
+			<span className='truncate'>Manage Guilds</span>
+		</Link>
+	);
+}
