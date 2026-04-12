86fb77fd41f123ed93691b8e54efdd40dbfe6d80
JJSosaL <hello@fancystudio.xyz>
2026-04-12 20:53:05 +0200

    feat(utils): add `getGuild`

---
+++ apps/vanguard-dashboard/app/.server/utils/API/getGuild.ts
@@ -0,0 +1,38 @@
+import { redirect } from 'react-router';
+import { match } from 'ts-pattern';
+import { BASE_API_URL } from '#server/lib/Constants/Shared.ts';
+import type { Guild } from '#server/lib/Types/API.ts';
+import { getCookieHeader } from '../Request/getCookieHeader.ts';
+
+export async function getGuild(
+	request: Request,
+	guildId: string,
+): Promise<Guild> {
+	const response = await createRequest(request, guildId);
+
+	return await match(response)
+		.returnType<Promise<Guild>>()
+		.with(
+			{
+				ok: true,
+			},
+			async () => await response.json(),
+		)
+		.otherwise(() => {
+			throw redirect(`${BASE_API_URL}/api/guilds/${guildId}/invite`);
+		});
+}
+
+async function createRequest(
+	request: Request,
+	guildId: string,
+): Promise<Response> {
+	const cookie = getCookieHeader(request);
+	const response = await fetch(`${BASE_API_URL}/api/guilds/${guildId}`, {
+		headers: {
+			cookie,
+		},
+	});
+
+	return response;
+}
