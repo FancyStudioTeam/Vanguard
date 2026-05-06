a2cd8dadd269b2927dc196dd548d70db34904e7a
JJSosaL <hello@fancystudio.xyz>
2026-05-06 22:41:49 +0200

    refactor: use `SessionGuard`

--- apps/vanguard-api/src/modules/User/User.controller.ts
+++ apps/vanguard-api/src/modules/User/User.controller.ts
@@ -1,11 +1,12 @@
-import { Controller, Get, Inject, Session } from '@nestjs/common';
+import { Controller, Get, Inject, UseGuards } from '@nestjs/common';
 
-import { UNAUTHORIZED_RESPONSE } from '#lib/Responses/Shared.js';
-import type { FastifySession } from '#lib/Types/Fastify.js';
+import { SessionId } from '#common/Decorators/SessionId.js';
+import { SessionGuard } from '#common/Guards/SessionGuard.js';
 import { DiscordService } from '#modules/Discord/Discord.service.js';
 import { SessionsService } from '#modules/Sessions/Sessions.service.js';
 
 @Controller('users/@me')
+@UseGuards(SessionGuard)
 export class UserController {
 	public constructor(
 		@Inject(DiscordService) private readonly discordService: DiscordService,
@@ -13,14 +14,7 @@ export class UserController {
 	) {}
 
 	@Get()
-	public async getCurrentUser(@Session() fastifySession: FastifySession) {
-		const sessionId = fastifySession.get('sessionId');
-		const sessionUserId = fastifySession.get('sessionUserId');
-
-		if (!(sessionId && sessionUserId)) {
-			throw UNAUTHORIZED_RESPONSE();
-		}
-
+	public async getCurrentUser(@SessionId() sessionId: string) {
 		const currentUserAccessToken = await this.sessionsService.getAccessToken(sessionId);
 		const currentUser = await this.discordService.getCurrentUser(currentUserAccessToken);
 
