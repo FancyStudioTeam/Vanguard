f8ff1580fe83617205752d2ae9ceec002f574d26
JJSosaL <hello@fancystudio.xyz>
2026-05-06 20:25:59 +0200

    docs: explain about environment variables

--- apps/vanguard-api/src/env.ts
+++ apps/vanguard-api/src/env.ts
@@ -2,6 +2,13 @@ import { env, loadEnvFile } from 'node:process';
 
 const { NODE_ENV } = env;
 
+/*
+ * In Docker, environment variables are commonly injected using '--env-file'
+ * or via 'docker-compose'.
+ *
+ * The '.env' file is not included in the Docker image, so attempting to load
+ * it at runtime may fail.
+ */
 if (NODE_ENV === 'development') {
 	loadEnvFile();
 }
