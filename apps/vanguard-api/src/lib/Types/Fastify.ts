cc55086e7062b20620cbb8d99844e9ad2f285aa1
JJSosaL <hello@fancystudio.xyz>
2026-05-06 22:35:06 +0200

    refactor: delete `sessionUser` from session

--- apps/vanguard-api/src/lib/Types/Fastify.ts
+++ apps/vanguard-api/src/lib/Types/Fastify.ts
@@ -1,12 +1,8 @@
 import type { Session as FastifySecureSession } from '@fastify/secure-session';
 
-import type { User } from './Discord.js';
-
 export interface FastifySessionData {
 	sessionId: string;
-	sessionUser: FastifySessionUser;
 	sessionUserId: string;
 }
 
 export type FastifySession = FastifySecureSession<FastifySessionData>;
-export type FastifySessionUser = User;
