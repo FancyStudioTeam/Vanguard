3991323264f0588473cd396249c5cf7dd757c059
JJSosaL <hello@fancystudio.xyz>
2026-05-07 17:03:35 +0200

    refactor: `Dockerfile`

--- apps/vanguard-client/src/env.ts
+++ apps/vanguard-client/src/env.ts
@@ -1,7 +1,18 @@
-import { env, loadEnvFile } from 'node:process';
+import { env } from 'node:process';
+
+import { configDotenv } from 'dotenv';
 
 const { NODE_ENV } = env;
 
+/*
+ * In Docker, environment variables are commonly injected using '--env-file'
+ * or via 'docker-compose'.
+ *
+ * The '.env' file is not included in the Docker image, so attempting to load
+ * it at runtime may fail.
+ */
 if (NODE_ENV === 'development') {
-	loadEnvFile();
+	configDotenv({
+		quiet: true,
+	});
 }
