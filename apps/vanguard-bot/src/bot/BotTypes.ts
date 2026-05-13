a1361955ce0d315c83fe5f0c4a75ddc3cd202d12
JJSosaL <hello@fancystudio.xyz>
2026-05-13 22:10:05 +0200

    refactor: delete unused bot types

--- apps/vanguard-bot/src/bot/BotTypes.ts
+++ apps/vanguard-bot/src/bot/BotTypes.ts
@@ -7,10 +7,3 @@ export interface Bot extends InferredBot {
 	commandManager: CommandManager;
 	eventManager: EventManager;
 }
-
-export type BotEventNames = keyof BotEvents;
-export type BotEvents = Bot['events'];
-
-export type BotInferredTypes = BotTransformers['$inferredTypes'];
-
-export type BotTransformers = Bot['transformers'];
