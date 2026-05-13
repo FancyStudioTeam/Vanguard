37aeef9ad590b1db7fc3feea0cca62e6bd755224
JJSosaL <hello@fancystudio.xyz>
2026-05-13 16:23:01 +0200

    fix: delete `/client`

--- apps/vanguard-api/src/modules/Prisma/Prisma.service.ts
+++ apps/vanguard-api/src/modules/Prisma/Prisma.service.ts
@@ -1,4 +1,4 @@
-import { PrismaAdapter, PrismaClient } from '@vanguard/prisma/client';
+import { PrismaAdapter, PrismaClient } from '@vanguard/prisma';
 
 import { Injectable } from '@nestjs/common';
 
