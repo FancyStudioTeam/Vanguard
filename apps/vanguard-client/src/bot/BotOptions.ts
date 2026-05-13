c2f3821fb6180d1e988362045b73132c14d2763f
JJSosaL <hello@fancystudio.xyz>
2026-05-13 19:39:56 +0200

    refactor: export `DESIRED_PROPERTIES` object

--- apps/vanguard-client/src/bot/BotOptions.ts
+++ apps/vanguard-client/src/bot/BotOptions.ts
@@ -1,25 +1,12 @@
 import { platform } from 'node:process';
 
-import {
-	DESIRED_GUILD_PROPERTIES,
-	DESIRED_INTERACTION_PROPERTIES,
-	DESIRED_MESSAGE_PROPERTIES,
-	DESIRED_PROPERTIES_BEHAVIOR,
-	DESIRED_USER_PROPERTIES,
-	type DesiredProperties,
-} from '@vanguard/discord-desired-properties';
+import { DESIRED_PROPERTIES, DESIRED_PROPERTIES_BEHAVIOR } from '@vanguard/discord-desired-properties';
 
 import { ActivityTypes, type CreateGatewayManagerOptions } from '@discordeno/bot';
 
 import { CLIENT_GATEWAY_INTENTS, CLIENT_TOKEN } from '#lib/Constants.js';
 
-export const BOT_DESIRED_PROPERTIES = {
-	guild: DESIRED_GUILD_PROPERTIES,
-	interaction: DESIRED_INTERACTION_PROPERTIES,
-	message: DESIRED_MESSAGE_PROPERTIES,
-	user: DESIRED_USER_PROPERTIES,
-} satisfies BotDesiredProperties;
-
+export const BOT_DESIRED_PROPERTIES = DESIRED_PROPERTIES;
 export const BOT_DESIRED_PROPERTIES_BEHAVIOR = DESIRED_PROPERTIES_BEHAVIOR;
 
 export const BOT_GATEWAY_MANAGER_PROPERTIES = {
@@ -47,8 +34,6 @@ export const BOT_GATEWAY_MANAGER = {
 
 export const BOT_INTENTS = CLIENT_GATEWAY_INTENTS;
 export const BOT_TOKEN = CLIENT_TOKEN;
-
-type BotDesiredProperties = DesiredProperties;
 
 type BotGatewayManager = Omit<CreateGatewayManagerOptions, 'token'>;
 type BotGatewayManagerProperties = BotGatewayManager['properties'];
--- packages/discord-desired-properties/src/index.ts
+++ packages/discord-desired-properties/src/index.ts
@@ -1,7 +1,5 @@
 import { DesiredPropertiesBehavior, type RecursivePartial, type TransformersDesiredProperties } from '@discordeno/bot';
 
-export const DESIRED_PROPERTIES_BEHAVIOR = DesiredPropertiesBehavior.RemoveKey;
-
 export const DESIRED_GUILD_PROPERTIES = {
 	icon: true,
 	id: true,
@@ -32,6 +30,15 @@ export const DESIRED_USER_PROPERTIES = {
 	toggles: true,
 	username: true,
 } satisfies DesiredUserProperties;
+
+export const DESIRED_PROPERTIES = {
+	guild: DESIRED_GUILD_PROPERTIES,
+	interaction: DESIRED_INTERACTION_PROPERTIES,
+	message: DESIRED_MESSAGE_PROPERTIES,
+	user: DESIRED_USER_PROPERTIES,
+} as const satisfies DesiredProperties;
+
+export const DESIRED_PROPERTIES_BEHAVIOR = DesiredPropertiesBehavior.RemoveKey;
 
 export type DesiredProperties = RecursivePartial<TransformersDesiredProperties>;
 
