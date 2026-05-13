ec16bcd3282da69984e54c5b27d81aef8603af34
JJSosaL <hello@fancystudio.xyz>
2026-05-13 22:23:03 +0200

    refactor: delete unused functions

--- apps/vanguard-bot/src/utils/defineReadonlyProperty.ts
+++
@@ -1,8 +0,0 @@
-export function defineReadonlyProperty(object: object, name: string, value: unknown): void {
-	Reflect.defineProperty(object, name, {
-		configurable: false,
-		enumerable: false,
-		value,
-		writable: false,
-	});
-}
--- apps/vanguard-bot/src/utils/getEnvFileName.ts
+++
@@ -1,7 +0,0 @@
-import { isProductionEnvironment } from './isProductionEnvironment.js';
-
-export function getEnvFileName(): EnvironmentFileName {
-	return isProductionEnvironment() ? '.env.production' : '.env.development';
-}
-
-type EnvironmentFileName = '.env.production' | '.env.development';
