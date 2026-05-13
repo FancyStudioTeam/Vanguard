5dd8a5d9dab35ae5f51ec22931a89192712bb8c5
JJSosaL <hello@fancystudio.xyz>
2026-05-13 19:59:24 +0200

    fix: add `CompleteDesiredProperties`

--- packages/discord-handlers/src/types/Bot.ts
+++ packages/discord-handlers/src/types/Bot.ts
@@ -1,9 +1,8 @@
 import type { DESIRED_PROPERTIES, DESIRED_PROPERTIES_BEHAVIOR } from '@vanguard/discord-desired-properties';
 
-import type { Bot as DiscordenoBot } from '@discordeno/bot';
+import type { CompleteDesiredProperties, Bot as DiscordenoBot } from '@discordeno/bot';
 
-// @ts-expect-error
-export type Bot = DiscordenoBot<typeof DESIRED_PROPERTIES, typeof DESIRED_PROPERTIES_BEHAVIOR>;
+export type Bot = DiscordenoBot<CompleteDesiredProperties<typeof DESIRED_PROPERTIES>, typeof DESIRED_PROPERTIES_BEHAVIOR>;
 
 export type BotEventNames = keyof BotEvents;
 export type BotEvents = Bot['events'];
