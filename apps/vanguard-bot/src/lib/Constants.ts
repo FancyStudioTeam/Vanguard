b68b0ead687dc979034a333e4075c1d3758060c3
JJSosaL <hello@fancystudio.xyz>
2026-05-14 21:58:37 +0200

    chore: add `GuildVoiceStates`

--- apps/vanguard-bot/src/lib/Constants.ts
+++ apps/vanguard-bot/src/lib/Constants.ts
@@ -4,7 +4,11 @@ import { getEnvVariable } from '#utils/getEnvVariable.js';
 import { getPackageVersion } from '#utils/getPackageVersion.js';
 
 export const CLIENT_GATEWAY_INTENTS =
-	GatewayIntents.GuildMembers | GatewayIntents.GuildMessages | GatewayIntents.Guilds | GatewayIntents.MessageContent;
+	GatewayIntents.GuildMembers |
+	GatewayIntents.GuildMessages |
+	GatewayIntents.GuildVoiceStates |
+	GatewayIntents.Guilds |
+	GatewayIntents.MessageContent;
 export const CLIENT_TOKEN = getEnvVariable('CLIENT_TOKEN');
 
 export const VERSION = getPackageVersion();
