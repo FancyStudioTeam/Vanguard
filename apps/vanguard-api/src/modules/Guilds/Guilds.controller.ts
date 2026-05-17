07f3e1a08a88c2f59b075a3c61bd104f7b2fa35a
JJSosaL <hello@fancystudio.xyz>
2026-05-17 02:05:46 +0200

    refactor: `GuildsModule`

--- apps/vanguard-api/src/modules/Guilds/Guilds.controller.ts
+++ apps/vanguard-api/src/modules/Guilds/Guilds.controller.ts
@@ -5,14 +5,14 @@ import { Controller, Get, Inject, UseGuards } from '@nestjs/common';
 import { SessionId } from '#common/Decorators/SessionId.js';
 import { SessionUserId } from '#common/Decorators/SessionUserId.js';
 import { SessionGuard } from '#common/Guards/SessionGuard.js';
+import { DiscordService } from '#modules/Discord/Discord.service.js';
 import { SessionsService } from '#modules/Sessions/Sessions.service.js';
-import { GuildsService } from './Guilds.service.js';
 
 @Controller()
 @UseGuards(SessionGuard(false))
 export class GuildsController {
 	public constructor(
-		@Inject(GuildsService) private readonly guildsService: GuildsService,
+		@Inject(DiscordService) private readonly discordService: DiscordService,
 		@Inject(SessionsService) private readonly sessionsService: SessionsService,
 	) {}
 
@@ -22,7 +22,7 @@ export class GuildsController {
 		@SessionUserId() sessionUserId: string,
 	): Promise<RESTGetAPIUserGuildsResponse> {
 		const currentUserAccessToken = await this.sessionsService.getAccessToken(sessionId);
-		const currentUserGuilds = await this.guildsService.getCurrentUserGuilds(sessionUserId, currentUserAccessToken);
+		const currentUserGuilds = await this.discordService.getCurrentUserGuilds(sessionUserId, currentUserAccessToken);
 
 		return currentUserGuilds;
 	}
--- apps/vanguard-api/src/modules/Guilds/Guilds.module.ts
+++ apps/vanguard-api/src/modules/Guilds/Guilds.module.ts
@@ -1,11 +1,13 @@
 import { CacheModule } from '@nestjs/cache-manager';
 import { Module } from '@nestjs/common';
+import { RouterModule } from '@nestjs/core';
 
 import { DiscordModule } from '#modules/Discord/Discord.module.js';
 import { DiscordUtilsModule } from '#modules/DiscordUtils/DiscordUtils.module.js';
 import { SessionsModule } from '#modules/Sessions/Sessions.module.js';
+import { GuildModule } from './Guild/Guild.module.js';
+import { TicketsModule } from './Guild/Tickets/Tickets.module.js';
 import { GuildsController } from './Guilds.controller.js';
-import { GuildsService } from './Guilds.service.js';
 
 @Module({
 	controllers: [
@@ -15,10 +17,24 @@ import { GuildsService } from './Guilds.service.js';
 		CacheModule.register(),
 		DiscordModule,
 		DiscordUtilsModule,
+		RouterModule.register([
+			{
+				children: [
+					{
+						children: [
+							{
+								module: TicketsModule,
+								path: 'tickets',
+							},
+						],
+						module: GuildModule,
+						path: ':guildId',
+					},
+				],
+				path: 'guilds',
+			},
+		]),
 		SessionsModule,
-	],
-	providers: [
-		GuildsService,
 	],
 })
 export class GuildsModule {}
