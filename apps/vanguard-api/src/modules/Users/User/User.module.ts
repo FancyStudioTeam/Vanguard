9f98b55d93c28d377afe4f66e26844dce860869a
JJSosaL <hello@fancystudio.xyz>
2026-05-17 02:07:19 +0200

    fix: missing `DiscordUtilsModule` import

--- apps/vanguard-api/src/modules/Users/User/User.module.ts
+++ apps/vanguard-api/src/modules/Users/User/User.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 
 import { DiscordModule } from '#modules/Discord/Discord.module.js';
+import { DiscordUtilsModule } from '#modules/DiscordUtils/DiscordUtils.module.js';
 import { SessionsModule } from '#modules/Sessions/Sessions.module.js';
 import { UserController } from './User.controller.js';
 
@@ -10,6 +11,7 @@ import { UserController } from './User.controller.js';
 	],
 	imports: [
 		DiscordModule,
+		DiscordUtilsModule,
 		SessionsModule,
 	],
 })
