c7a6a3f1203bc5c14a70eddc5bb5929b60e393a9
JJSosaL <hello@fancystudio.xyz>
2026-05-17 12:40:31 +0200

    fix(Biome): update naming convention

--- apps/vanguard-dashboard/react-router.config.ts
+++ apps/vanguard-dashboard/react-router.config.ts
@@ -2,7 +2,6 @@ import type { Config } from '@react-router/dev/config';
 
 export default {
 	future: {
-		// biome-ignore lint/style/useNamingConvention: (x)
 		v8_middleware: true,
 	},
 	ssr: true,
