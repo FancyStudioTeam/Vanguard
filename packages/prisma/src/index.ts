f914a7a47ffb40a46b7cb0704656aa9634e6134c
JJSosaL <hello@fancystudio.xyz>
2026-05-17 13:37:38 +0200

    refactor: export everything from `prisma/client.js`

--- packages/prisma/src/index.ts
+++ packages/prisma/src/index.ts
@@ -1,8 +1,7 @@
 import { PrismaPg } from '@prisma/adapter-pg';
 import { env } from 'prisma/config';
 
-// biome-ignore lint/performance/noBarrelFile: (x)
-export { PrismaClient } from './generated/prisma/client.js';
+export * from './generated/prisma/client.js';
 export type * from './generated/prisma/enums.js';
 export type * from './generated/prisma/models.js';
 
