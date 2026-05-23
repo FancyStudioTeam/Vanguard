7e22ba17bcb709f94cdc0299345dc3291572cf68
JJSosaL <hello@fancystudio.xyz>
2026-05-23 17:10:17 +0200

    refactor: delete `export type`

--- packages/prisma/src/index.ts
+++ packages/prisma/src/index.ts
@@ -2,8 +2,8 @@ import { PrismaPg } from '@prisma/adapter-pg';
 import { env } from 'prisma/config';
 
 export * from './generated/prisma/client.js';
-export type * from './generated/prisma/enums.js';
-export type * from './generated/prisma/models.js';
+export * from './generated/prisma/enums.js';
+export * from './generated/prisma/models.js';
 
 export const PrismaAdapter = new PrismaPg({
 	connectionString: env('POSTGRE_SQL_DATABASE_URL'),
