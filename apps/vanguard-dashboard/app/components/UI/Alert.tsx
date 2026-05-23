05d64dac54de9e309fc7540e2e94ba66cd9b182c
JJSosaL <hello@fancystudio.xyz>
2026-05-23 12:47:07 +0200

    refactor: update `PanelsEmptyState`

--- apps/vanguard-dashboard/app/components/UI/Alert.tsx
+++ apps/vanguard-dashboard/app/components/UI/Alert.tsx
@@ -2,24 +2,29 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import type { ComponentProps } from 'react';
 import { twMerge } from 'tailwind-merge';
 
-const alertVariants = cva('flex items-center gap-2 rounded-xl border-2 border-neutral-800 bg-neutral-900 p-4', {
-	variants: {
-		variant: {
-			amber: 'text-amber-400',
-			cyan: 'text-cyan-400',
-			default: 'text-neutral-400',
-			emerald: 'text-emerald-400',
-			rose: 'text-rose-400',
+const AlertVariants = cva(
+	'flex gap-2 rounded-xl border-2 border-neutral-800 bg-neutral-900 p-4 [&>svg]:top-0 [&>svg]:size-5 [&>svg]:shrink-0',
+	{
+		defaultVariants: {
+			variant: 'default',
+		},
+		variants: {
+			variant: {
+				amber: 'text-amber-400',
+				cyan: 'text-cyan-400',
+				default: 'text-neutral-400',
+				emerald: 'text-emerald-400',
+				rose: 'text-rose-400',
+			},
 		},
 	},
-});
+);
 
-export function Alert({ className, variant = 'default', ...props }: AlertProps) {
+export function Alert({ className, ...props }: AlertProps) {
 	return (
 		<div
-			className={alertVariants({
+			className={AlertVariants({
 				className,
-				variant,
 			})}
 			{...props}
 		/>
@@ -29,11 +34,11 @@ export function Alert({ className, variant = 'default', ...props }: AlertProps) 
 export function AlertDescription({ className, ...props }: AlertDescriptionProps) {
 	return (
 		<span
-			className={twMerge('text-balance text-sm', className)}
+			className={twMerge('text-balance font-bold text-sm', className)}
 			{...props}
 		/>
 	);
 }
 
 export type AlertDescriptionProps = ComponentProps<'span'>;
-export type AlertProps = ComponentProps<'div'> & VariantProps<typeof alertVariants>;
+export type AlertProps = ComponentProps<'div'> & VariantProps<typeof AlertVariants>;
--- apps/vanguard-dashboard/app/routes/Guild/routes/Tickets/components/Panels/PanelsEmptyState.tsx
+++ apps/vanguard-dashboard/app/routes/Guild/routes/Tickets/components/Panels/PanelsEmptyState.tsx
@@ -1,15 +1,15 @@
-import { InfoIcon } from '@phosphor-icons/react';
-
-import { Alert, AlertDescription } from '#components/UI/Alert.tsx';
+import { TicketIcon } from '@phosphor-icons/react';
 
 export function PanelsEmptyState() {
 	return (
-		<Alert>
-			<InfoIcon
-				className='size-5 shrink-0'
-				weight='fill'
-			/>
-			<AlertDescription>This server does not have any panel created.</AlertDescription>
-		</Alert>
+		<section className='flex h-75 items-center justify-center rounded-xl border-2 border-neutral-800 border-dashed p-6'>
+			<section className='flex w-full max-w-xs flex-col gap-2 text-center'>
+				<section className='mx-auto size-min rounded-md bg-neutral-800 p-2 [&>svg]:size-7'>
+					<TicketIcon weight='fill' />
+				</section>
+				<h2 className='font-bold text-md'>No Ticket Panels Yet</h2>
+				<p className='text-neutral-400 text-sm'>You can start by creating a panel to manage your tickets.</p>
+			</section>
+		</section>
 	);
 }
