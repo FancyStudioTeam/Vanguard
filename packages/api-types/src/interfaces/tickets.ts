fce97c2872372b135f8e3024dde9507c4d906d16
JJSosaL <hello@fancystudio.xyz>
2026-05-23 17:11:43 +0200

    refactor: use `GuildTicketPanelType`

--- packages/api-types/src/interfaces/tickets.ts
+++ packages/api-types/src/interfaces/tickets.ts
@@ -1,15 +1,19 @@
 // biome-ignore-all lint/style/useNamingConvention: (x)
 
+import { GuildTicketPanelType } from '@vanguard/prisma';
+
 export interface APIGuildTicketPanel {
 	channel_id: string;
 	channel_parent_id: string | null;
 	enabled: boolean;
 	panel_id: string;
 	title: string;
-	type: unknown;
+	type: GuildTicketPanelType;
 }
 
 export interface APIGuildTicketsConfiguration {
 	enabled: boolean;
 	panels: APIGuildTicketPanel[];
 }
+
+export { GuildTicketPanelType };
