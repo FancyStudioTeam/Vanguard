dfcb151e016ecc7d81ae1919c6208472f0db634d
JJSosaL <hello@fancystudio.xyz>
2026-05-23 12:30:16 +0200

    fix: delete `encodeURIComponent`

--- apps/vanguard-dashboard/app/utils/URL/createRequestEndpoint.ts
+++ apps/vanguard-dashboard/app/utils/URL/createRequestEndpoint.ts
@@ -1,5 +1,5 @@
 import { BASE_API_URL } from '#lib/Shared.ts';
 
 export function createRequestUrl(endpoint: string): string {
-	return `${BASE_API_URL}/api/${encodeURIComponent(endpoint)}`;
+	return `${BASE_API_URL}/api/${endpoint}`;
 }
