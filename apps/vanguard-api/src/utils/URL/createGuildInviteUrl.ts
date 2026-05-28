3c91ba5eece36a8410f5e51de0346ecd4cb1fc64
JJSosaL <hello@fancystudio.xyz>
2026-05-28 19:09:58 +0200

    refactor: `createRedirectUrl` and `createGuildInviteUrl`

--- apps/vanguard-api/src/utils/URL/createGuildInviteUrl.ts
+++ apps/vanguard-api/src/utils/URL/createGuildInviteUrl.ts
@@ -1,26 +1,31 @@
-import { OAuth2Scope } from '@discordeno/types';
-import { createOAuth2Link } from '@discordeno/utils';
+import { OAuth2Routes, OAuth2Scopes, PermissionFlagsBits } from 'discord-api-types/v10';
 
 import { CLIENT_ID } from '#lib/Constants/Client.js';
 
+/**
+ * @see https://docs.discord.com/developers/topics/oauth2#bot-authorization-flow
+ */
 export function createGuildInviteUrl(guildId: string): string {
-	return createOAuth2Link({
-		clientId: CLIENT_ID,
-		disableGuildSelect: true,
-		guildId,
-		permissions: [
-			'EMBED_LINKS',
-			'MANAGE_GUILD',
-			'MANAGE_THREADS',
-			'MODERATE_MEMBERS',
-			'READ_MESSAGE_HISTORY',
-			'SEND_MESSAGES',
-			'SEND_MESSAGES_IN_THREADS',
-			'USE_EXTERNAL_EMOJIS',
-			'VIEW_CHANNEL',
-		],
-		scope: [
-			OAuth2Scope.Bot,
-		],
-	});
+	const url = new URL(OAuth2Routes.authorizationURL);
+	const urlSearchParams = url.searchParams;
+
+	urlSearchParams.append('client_id', CLIENT_ID);
+	urlSearchParams.append('guild_id', guildId);
+
+	urlSearchParams.append('disable_guild_select', String(true));
+
+	urlSearchParams.append('scope', OAuth2Scopes.Bot);
+
+	urlSearchParams.append(
+		'permissions',
+		String(
+			PermissionFlagsBits.EmbedLinks |
+				PermissionFlagsBits.ManageGuild |
+				PermissionFlagsBits.ReadMessageHistory |
+				PermissionFlagsBits.SendMessages |
+				PermissionFlagsBits.ViewChannel,
+		),
+	);
+
+	return url.toString();
 }
--- apps/vanguard-api/src/utils/URL/createRedirectUrl.ts
+++ apps/vanguard-api/src/utils/URL/createRedirectUrl.ts
@@ -1,23 +1,31 @@
-import { OAuth2Scope } from '@discordeno/types';
-import { createOAuth2Link } from '@discordeno/utils';
+import { OAuth2Routes, OAuth2Scopes } from 'discord-api-types/v10';
 
 import { CLIENT_ID } from '#lib/Constants/Client.js';
 import { createCallbackUrl } from './createCallbackUrl.js';
 
+/**
+ * @see https://docs.discord.com/developers/topics/oauth2#authorization-code-grant
+ */
 export function createRedirectUrl(): string {
-	const callbackUrl = createCallbackUrl();
-	const redirectUrl = createOAuth2Link({
-		clientId: CLIENT_ID,
-		prompt: 'none',
-		redirectUri: callbackUrl,
-		responseType: 'code',
-		scope: [
-			OAuth2Scope.Email,
-			OAuth2Scope.Identify,
-			OAuth2Scope.Guilds,
-			OAuth2Scope.GuildsMembersRead,
-		],
-	});
+	const url = new URL(OAuth2Routes.authorizationURL);
+	const urlSearchParams = url.searchParams;
 
-	return redirectUrl;
+	urlSearchParams.append('client_id', CLIENT_ID);
+
+	urlSearchParams.append('response_type', 'code');
+	urlSearchParams.append('prompt', 'none');
+
+	urlSearchParams.append(
+		'scope',
+		[
+			OAuth2Scopes.Email,
+			OAuth2Scopes.Identify,
+			OAuth2Scopes.Guilds,
+			OAuth2Scopes.GuildsMembersRead,
+		].join('%20'),
+	);
+
+	urlSearchParams.append('redirect_uri', createCallbackUrl());
+
+	return url.toString();
 }
