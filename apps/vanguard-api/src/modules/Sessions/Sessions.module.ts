650fac5821d98bd38a0a77fb546f01e1860eeb35
JJSosaL <hello@fancystudio.xyz>
2026-06-01 22:35:43 +0200

    refactor!: `SessionsModule`

--- apps/vanguard-api/src/modules/Sessions/Sessions.module.ts
+++ apps/vanguard-api/src/modules/Sessions/Sessions.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { Session, SessionSchema } from '#schemas/Mongoose/Session.js';
+import {
+	SESSIONS_DATABASE_HOST,
+	SESSIONS_DATABASE_NAME,
+	SESSIONS_DATABASE_PORT,
+	SESSIONS_DATABASE_USER_NAME,
+	SESSIONS_DATABASE_USER_PASSWORD,
+} from '#lib/Constants/Sessions.js';
 import { EncryptionModule } from '../Encryption/Encryption.module.js';
+import { SessionEntity } from './Entities/Session.entity.js';
+import { SessionCredentialsEntity } from './Entities/SessionCredentials.entity.js';
 import { SessionsService } from './Sessions.service.js';
 
 @Module({
@@ -11,13 +19,21 @@ import { SessionsService } from './Sessions.service.js';
 	],
 	imports: [
 		EncryptionModule,
-		MongooseModule.forFeature([
+		TypeOrmModule.forFeature(
+			[
+				SessionCredentialsEntity,
+				SessionEntity,
+			],
 			{
-				collection: 'sessions',
-				name: Session.name,
-				schema: SessionSchema,
+				database: SESSIONS_DATABASE_NAME,
+				host: SESSIONS_DATABASE_HOST,
+				password: SESSIONS_DATABASE_USER_PASSWORD,
+				port: Number(SESSIONS_DATABASE_PORT),
+				synchronize: true,
+				type: 'postgres',
+				username: SESSIONS_DATABASE_USER_NAME,
 			},
-		]),
+		),
 	],
 	providers: [
 		SessionsService,
