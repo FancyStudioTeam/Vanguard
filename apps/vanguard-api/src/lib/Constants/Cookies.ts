3baa1ceed8e508efa2b1b995baa8b9c360eba237
JJSosaL <hello@fancystudio.xyz>
2026-06-01 20:24:04 +0200

    refactor!: cookie constants

--- apps/vanguard-api/src/lib/Constants/Cookies.ts
+++ apps/vanguard-api/src/lib/Constants/Cookies.ts
@@ -1,7 +1,2 @@
-import { getEnvVariable } from '#utils/Process/getEnvVariable.js';
-
-export const COOKIE_SALT = getEnvVariable('COOKIE_SALT');
-export const COOKIE_SECRET = getEnvVariable('COOKIE_SECRET');
-
-export const COOKIE_SESSION_DATA_MAX_AGE = 86_400;
-export const COOKIE_SESSION_DATA_NAME = 'session_data';
+export const ACCESS_TOKEN_COOKIE_MAX_AGE = 86_400;
+export const ACCESS_TOKEN_COOKIE_NAME = 'access_token';
