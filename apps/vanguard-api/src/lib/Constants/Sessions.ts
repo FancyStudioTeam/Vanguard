dbb111ae34b19b08f3db7108ca8d90de46a9100d
JJSosaL <hello@fancystudio.xyz>
2026-06-01 22:18:46 +0200

    refactor: `SESSIONS_DATABASE_USER_NAME`

--- apps/vanguard-api/src/lib/Constants/Sessions.ts
+++ apps/vanguard-api/src/lib/Constants/Sessions.ts
@@ -3,4 +3,6 @@ import { getEnvVariable } from '#utils/Process/getEnvVariable.js';
 export const SESSIONS_DATABASE_HOST = getEnvVariable('SESSIONS_DATABASE_HOST');
 export const SESSIONS_DATABASE_NAME = getEnvVariable('SESSIONS_DATABASE_NAME');
 export const SESSIONS_DATABASE_PORT = getEnvVariable('SESSIONS_DATABASE_PORT');
-export const SESSIONS_DATABASE_USER = getEnvVariable('SESSIONS_DATABASE_USER');
+
+export const SESSIONS_DATABASE_USER_NAME = getEnvVariable('SESSIONS_DATABASE_USER_NAME');
+export const SESSIONS_DATABASE_USER_PASSWORD = getEnvVariable('SESSIONS_DATABASE_USER_PASSWORD');
