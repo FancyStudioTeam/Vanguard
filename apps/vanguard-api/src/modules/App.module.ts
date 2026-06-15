e985cf39ec910451f8c89a96bf8fb58ae3515807
JJSosaL <hello@fancystudio.xyz>
2026-06-15 20:56:51 +0200

    refactor!: `SessionsModule` is not longer required

--- apps/vanguard-api/src/modules/App.module.ts
+++ apps/vanguard-api/src/modules/App.module.ts
@@ -1,18 +1,10 @@
 import { type MiddlewareConsumer, Module, type NestModule } from '@nestjs/common';
 import { APP_FILTER, APP_GUARD, RouterModule } from '@nestjs/core';
 import { ScheduleModule } from '@nestjs/schedule';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { HttpExceptionFilter } from '#common/Filters/HttpExceptionFilter.js';
 import { AuthGuard } from '#common/Guards/AuthGuard.js';
 import { LoggerMiddleware } from '#common/Middlewares/LoggerMiddleware.js';
-import {
-	SESSIONS_DATABASE_HOST,
-	SESSIONS_DATABASE_NAME,
-	SESSIONS_DATABASE_PORT,
-	SESSIONS_DATABASE_USER_NAME,
-	SESSIONS_DATABASE_USER_PASSWORD,
-} from '#lib/Constants/Sessions.js';
 import { AuthModule } from './Auth/Auth.module.js';
 import { DiscordModule } from './Discord/Discord.module.js';
 import { GuildsModule } from './Guilds/Guilds.module.js';
@@ -30,16 +22,6 @@ import { UsersRoutes } from './Users/Users.routes.js';
 			UsersRoutes,
 		]),
 		ScheduleModule.forRoot(),
-		TypeOrmModule.forRoot({
-			autoLoadEntities: true,
-			database: SESSIONS_DATABASE_NAME,
-			host: SESSIONS_DATABASE_HOST,
-			password: SESSIONS_DATABASE_USER_PASSWORD,
-			port: Number(SESSIONS_DATABASE_PORT),
-			synchronize: true,
-			type: 'postgres',
-			username: SESSIONS_DATABASE_USER_NAME,
-		}),
 		UsersModule,
 	],
 	providers: [
