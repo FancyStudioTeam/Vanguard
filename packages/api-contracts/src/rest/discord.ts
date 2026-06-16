d2d1090ac956534c5a4754464869bfdc2d7383a5
JJSosaL <hello@fancystudio.xyz>
2026-06-16 18:48:07 +0200

    fix: delete unused api contract

--- packages/api-contracts/src/rest/discord.ts
+++ packages/api-contracts/src/rest/discord.ts
@@ -1,12 +1,6 @@
-import type {
-	DiscordGuild,
-	DiscordGuildChannel,
-	DiscordUser,
-	DiscordUserGuild,
-} from '#interfaces/discord.js';
+import type { DiscordGuild, DiscordUser, DiscordUserGuild } from '#interfaces/discord.js';
 
 export type GetDiscordGuildResult = DiscordGuild;
-export type GetDiscordGuildChannelsResult = DiscordGuildChannel[];
 
 export type GetDiscordUserResult = DiscordUser;
 export type GetDiscordUserGuildsResult = DiscordUserGuild[];
