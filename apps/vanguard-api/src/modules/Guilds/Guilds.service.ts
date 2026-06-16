6d532800f8d0bbb9107065bf1ec0fa3ac8afc02d
JJSosaL <hello@fancystudio.xyz>
2026-06-16 19:44:32 +0200

    feat: add `GuildsService`

---
+++ apps/vanguard-api/src/modules/Guilds/Guilds.service.ts
@@ -0,0 +1,31 @@
+import type { DiscordUserGuild } from '@vanguard/api-contracts/interfaces';
+
+import { Inject, Injectable } from '@nestjs/common';
+
+import type { FastifySession } from '#lib/Types/Fastify.js';
+import { AuthService } from '#modules/Auth/Auth.service.js';
+import { DiscordService } from '#modules/Discord/Discord.service.js';
+import { ParserService } from '#modules/Parser/Parser.service.js';
+
+@Injectable()
+export class GuildsService {
+	public constructor(
+		@Inject(AuthService) private readonly authService: AuthService,
+		@Inject(DiscordService) private readonly discordService: DiscordService,
+		@Inject(ParserService) private readonly parserService: ParserService,
+	) {}
+
+	public async getCurrentUserGuilds(fastifySession: FastifySession): Promise<DiscordUserGuild[]> {
+		const accessToken = this.authService.getAccessToken(fastifySession);
+		const userId = fastifySession.get('userId') ?? '';
+
+		const currentUserGuilds = await this.discordService.getCurrentUserGuilds(
+			userId,
+			accessToken,
+		);
+		const currentUserGuildsParsed =
+			this.parserService.parseDiscordUserGuilds(currentUserGuilds);
+
+		return currentUserGuildsParsed;
+	}
+}
