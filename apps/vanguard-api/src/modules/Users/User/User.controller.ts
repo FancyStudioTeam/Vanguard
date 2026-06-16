7a37507ee48d1bf5142341f7549b7209d4014520
JJSosaL <hello@fancystudio.xyz>
2026-06-16 19:41:22 +0200

    feat: add status code

--- apps/vanguard-api/src/modules/Users/User/User.controller.ts
+++ apps/vanguard-api/src/modules/Users/User/User.controller.ts
@@ -1,6 +1,6 @@
 import type { GetDiscordUserResult } from '@vanguard/api-contracts/rest';
 
-import { Controller, Get, Inject, Session } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus, Inject, Session } from '@nestjs/common';
 
 import { BypassGuildPermissions } from '#common/Decorators/BypassGuildPermissionsKey.js';
 import type { FastifySession } from '#lib/Types/Fastify.js';
@@ -12,6 +12,7 @@ export class UserController {
 	public constructor(@Inject(UserService) private readonly userService: UserService) {}
 
 	@Get()
+	@HttpCode(HttpStatus.OK)
 	protected async getCurrentUser(
 		@Session() fastifySession: FastifySession,
 	): Promise<GetDiscordUserResult> {
