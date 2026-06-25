8e5588825a6d55f732822b116a898f6090a41dff
JJSosaL <hello@fancystudio.xyz>
2026-06-26 00:51:28 +0200

    refactor: delete scroll animation

--- apps/vanguard-dashboard/app/layouts/PageLayout.tsx
+++ apps/vanguard-dashboard/app/layouts/PageLayout.tsx
@@ -1,33 +1,14 @@
-import { useGSAP } from '@gsap/react';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import { type ReactNode, useRef } from 'react';
+import type { ReactNode } from 'react';
 
 import { Navbar } from '#components/Layout/Navbar/Navbar.tsx';
 
 export function PageLayout({ children }: PageLayoutProps) {
-	const headerReference = useRef<HTMLElement>(null);
-
-	useGSAP(() => {
-		const headerReferenceValue = headerReference.current;
-
-		ScrollTrigger.create({
-			start: 10,
-			toggleClass: {
-				className: 'bg-neutral-950',
-				targets: headerReferenceValue,
-			},
-		});
-	});
-
 	return (
 		<>
-			<header
-				className='fixed top-0 z-10 w-full items-center'
-				ref={headerReference}
-			>
+			<header className='fixed top-0 z-10 w-full items-center backdrop-blur-xl'>
 				<Navbar />
 			</header>
-			<main>{children}</main>
+			{children}
 		</>
 	);
 }
