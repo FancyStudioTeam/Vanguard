2bc874b247fb69b7735886f1527804eea4b5198c
JJSosaL <hello@fancystudio.xyz>
2026-06-25 21:23:42 +0200

    refactor: `ButtonVariants`

--- apps/vanguard-dashboard/app/components/UI/Button.tsx
+++ apps/vanguard-dashboard/app/components/UI/Button.tsx
@@ -3,7 +3,7 @@ import { cva, type VariantProps } from 'class-variance-authority';
 
 // biome-ignore lint/style/useComponentExportOnlyModules: (x)
 export const ButtonVariants = cva(
-	'flex cursor-pointer items-center justify-center gap-2 rounded-md text-sm transition-colors [&>svg]:size-5 [&>svg]:shrink-0',
+	'flex h-10 cursor-pointer items-center justify-center gap-2 rounded-full border-2 border-transparent font-semibold text-sm transition-colors [&>svg]:size-5 [&>svg]:shrink-0',
 	{
 		defaultVariants: {
 			size: 'default',
@@ -15,10 +15,8 @@ export const ButtonVariants = cva(
 				icon: 'p-2',
 			},
 			variant: {
-				danger: 'bg-rose-900/75 text-rose-300 hover:bg-rose-900/50',
-				ghost: 'bg-transparent hover:bg-neutral-800',
-				primary: 'bg-neutral-800 hover:bg-neutral-700',
-				success: 'bg-emerald-900/75 text-emerald-300 hover:bg-emerald-900/50',
+				outline: '!border-neutral-800 hover:bg-neutral-800',
+				primary: 'bg-rose-950 text-rose-300 hover:bg-rose-950/75',
 			},
 		},
 	},
