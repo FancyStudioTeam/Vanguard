f3970cedd7c9f1bff1a02c60eb4d89d68cc500b9
JJSosaL <hello@fancystudio.xyz>
2026-06-27 15:25:00 +0200

    refactor: delete presence activities

--- apps/vanguard-bot/src/bot/BotOptions.ts
+++ apps/vanguard-bot/src/bot/BotOptions.ts
@@ -5,7 +5,7 @@ import {
 	DESIRED_PROPERTIES_BEHAVIOR,
 } from '@vanguard/discord-config/desired-properties';
 
-import { ActivityTypes, type CreateGatewayManagerOptions } from '@discordeno/bot';
+import type { CreateGatewayManagerOptions } from '@discordeno/bot';
 
 import { BOT_INTENTS, BOT_TOKEN } from '#lib/Constants.js';
 
@@ -20,17 +20,6 @@ export const BOT_GATEWAY_MANAGER_PROPERTIES = {
 
 export const BOT_GATEWAY_MANAGER = {
 	compress: true,
-	makePresence: async () => ({
-		activities: [
-			{
-				name: 'vanguard.fancystudio.xyz',
-				type: ActivityTypes.Watching,
-			},
-		],
-		afk: false,
-		since: null,
-		status: 'online',
-	}),
 	properties: BOT_GATEWAY_MANAGER_PROPERTIES,
 } satisfies BotGatewayManager;
 
