7008e74884911d2411e5e35b465f580e89fab964
JJSosaL <hello@fancystudio.xyz>
2026-06-28 01:17:21 +0200

    refactor: member previews

--- apps/vanguard-dashboard/app/routes/Guild/routes/Customization/components/Preview/PreviewMemberList.tsx
+++ apps/vanguard-dashboard/app/routes/Guild/routes/Customization/components/Preview/PreviewMemberList.tsx
@@ -1,30 +1,26 @@
 import { CheckFill } from '@mingcute/react';
 
-export function PreviewMemberList({ nickname }: PreviewMemberListProps) {
+export function PreviewMemberList({ avatarUrl, nickname }: PreviewMemberListProps) {
 	return (
 		<div className='flex items-center gap-2 rounded-3xl bg-neutral-800 p-2'>
 			<span className='relative shrink-0'>
 				<img
 					alt="Vanguard's Profile Icon"
 					className='size-10 rounded-full'
-					src='https://cdn.discordapp.com/avatars/1447604099465085010/63a8a4b7158c90ce2ea2e716af1b8b27.webp?size=1024'
+					src={avatarUrl}
 				/>
 				<span className='absolute right-0 bottom-0 size-3.5 rounded-full border-2 border-neutral-800 bg-emerald-500' />
 			</span>
-			<ul className='min-w-0'>
-				<li className='flex items-center gap-2 font-semibold'>
-					<span className='truncate'>{nickname}</span>
-					<span className='flex items-center gap-0.5 rounded-sm bg-indigo-500 px-1.5 py-0.5 text-xs uppercase'>
-						<CheckFill className='size-3' />
-						<span>App</span>
-					</span>
-				</li>
-				<li className='text-neutral-400 text-xs'>vanguard.fancystudio.xyz</li>
-			</ul>
+			<span className='truncate font-semibold'>{nickname ?? 'Vanguard'}</span>
+			<span className='flex items-center gap-0.5 rounded-sm bg-indigo-500 px-1.5 py-0.5 text-indigo-50 text-xs uppercase'>
+				<CheckFill className='size-3' />
+				<span>App</span>
+			</span>
 		</div>
 	);
 }
 
 export interface PreviewMemberListProps {
-	nickname: string;
+	avatarUrl: string;
+	nickname: string | null;
 }
--- apps/vanguard-dashboard/app/routes/Guild/routes/Customization/components/Preview/PreviewMemberProfile.tsx
+++ apps/vanguard-dashboard/app/routes/Guild/routes/Customization/components/Preview/PreviewMemberProfile.tsx
@@ -1,35 +1,51 @@
 import { CheckFill } from '@mingcute/react';
 
-export function PreviewMemberProfile({ biography, nickname }: PreviewMemberProfileProps) {
+export function PreviewMemberProfile({
+	avatarUrl,
+	bannerUrl,
+	biography,
+	nickname,
+}: PreviewMemberProfileProps) {
 	return (
 		<div className='overflow-hidden rounded-3xl bg-neutral-800'>
-			<header className='h-25 bg-rose-500' />
+			{bannerUrl ? (
+				<header
+					className='h-25 bg-center bg-cover bg-no-repeat'
+					style={{
+						backgroundImage: `url(${bannerUrl})`,
+					}}
+				/>
+			) : (
+				<header className='h-25 bg-rose-500' />
+			)}
 			<section className='flex -translate-y-10 flex-col gap-2 px-4'>
 				<span className='relative block size-fit'>
 					<img
 						alt="Vanguard's Profile Icon"
 						className='size-20 rounded-full border-4 border-neutral-800'
-						src='https://cdn.discordapp.com/avatars/1447604099465085010/63a8a4b7158c90ce2ea2e716af1b8b27.webp?size=1024'
+						src={avatarUrl}
 					/>
 					<span className='absolute right-0 bottom-0 size-7 rounded-full border-4 border-neutral-800 bg-emerald-500' />
 				</span>
-				<ul>
+				<ul className='min-w-0'>
 					<li className='flex items-center gap-2 font-semibold'>
-						<span className='truncate text-xl'>{nickname}</span>
-						<span className='flex items-center gap-0.5 rounded-sm bg-indigo-500 px-1.5 py-0.5 text-xs uppercase'>
+						<span className='truncate text-xl'>{nickname ?? 'Vanguard'}</span>
+						<span className='flex items-center gap-0.5 rounded-sm bg-indigo-500 px-1.5 py-0.5 text-indigo-50 text-xs uppercase'>
 							<CheckFill className='size-3' />
 							<span>App</span>
 						</span>
 					</li>
 					<li className='text-neutral-400 text-sm'>Vanguard#1075</li>
 				</ul>
-				<p className='break-all text-sm'>{biography}</p>
+				<p className='whitespace-normal text-sm'>{biography}</p>
 			</section>
 		</div>
 	);
 }
 
 export interface PreviewMemberProfileProps {
-	biography: string;
-	nickname: string;
+	avatarUrl: string;
+	bannerUrl: string | null;
+	biography: string | null;
+	nickname: string | null;
 }
