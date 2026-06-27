66cfd07bae0ed585cbeacccbb2c89c0f27063642
JJSosal <hello@fancystudio.xyz>
2026-06-27 05:16:41 +0200

    refactor: minor changes

--- apps/vanguard-dashboard/app/routes/Guild/components/Sidebar/SidebarHeader.tsx
+++ apps/vanguard-dashboard/app/routes/Guild/components/Sidebar/SidebarHeader.tsx
@@ -1,9 +1,5 @@
 import { VanguardCombinationMark } from '#components/Branding/VanguardCombinationMark.tsx';
 
 export function SidebarHeader() {
-	return (
-		<header className='grid place-content-center rounded-full bg-neutral-900 p-2'>
-			<VanguardCombinationMark />
-		</header>
-	);
+	return <VanguardCombinationMark className='w-min' />;
 }
