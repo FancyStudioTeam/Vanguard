0c3a1738b1f14bf36347cad7f83f9170490730fb
JJSosaL <hello@fancystudio.xyz>
2026-06-28 03:24:22 +0200

    refactor: `Editor`

--- apps/vanguard-dashboard/app/components/UI/Editor.tsx
+++ apps/vanguard-dashboard/app/components/UI/Editor.tsx
@@ -1,15 +1,49 @@
 import { yaml } from '@codemirror/lang-yaml';
 import { githubDark } from '@uiw/codemirror-theme-github';
-import CodeMirror from '@uiw/react-codemirror';
+import { createTheme } from '@uiw/codemirror-themes';
+import CodeMirror, { type BasicSetupOptions } from '@uiw/react-codemirror';
 
-export function Editor() {
+const CodeMirrorBasicSetup: BasicSetupOptions = {
+	bracketMatching: true,
+	indentOnInput: true,
+	lineNumbers: true,
+	tabSize: 2,
+};
+const CodeMirrorTheme = createTheme({
+	settings: {
+		background: '#171717',
+		caret: '#f43f5e',
+		fontFamily: 'JetBrains Mono',
+		fontSize: 16,
+		foreground: '#a3a3a3',
+		gutterBackground: '#171717',
+		gutterBorder: 'transparent',
+		gutterForeground: '#a3a3a3',
+		lineHighlight: '#262626',
+	},
+	styles: [],
+	theme: 'dark',
+});
+
+export function Editor({ code, onChange }: EditorProps) {
 	return (
 		<CodeMirror
+			basicSetup={CodeMirrorBasicSetup}
 			extensions={[
-				githubDark,
 				yaml(),
+				githubDark,
 			]}
-			height='200px'
+			height='auto'
+			minHeight='250px'
+			minWidth='100%'
+			onChange={onChange}
+			theme={CodeMirrorTheme}
+			value={code}
 		/>
 	);
+}
+
+export interface EditorProps {
+	code: string;
+	onChange: (value: string) => void;
 }
