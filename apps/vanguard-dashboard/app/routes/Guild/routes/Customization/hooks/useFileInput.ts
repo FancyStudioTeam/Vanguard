a53f0236f81e179f90350dcc38cec80c3f9d903d
JJSosaL <hello@fancystudio.xyz>
2026-06-30 21:03:41 +0200

    refactor: `useFileInput`

--- apps/vanguard-dashboard/app/routes/Guild/routes/Customization/hooks/useFileInput.ts
+++ apps/vanguard-dashboard/app/routes/Guild/routes/Customization/hooks/useFileInput.ts
@@ -1,34 +1,20 @@
-import {
-	type ChangeEvent,
-	type ChangeEventHandler,
-	type RefObject,
-	useCallback,
-	useRef,
-	useState,
-} from 'react';
+import { type ChangeEvent, type ChangeEventHandler, useCallback, useState } from 'react';
 
 export function useFileInput(): FileInputData {
 	const [file, setFile] = useState<File | null>(null);
-
-	const inputRef = useRef<HTMLInputElement>(null);
 
 	const onChange = useCallback(
 		({ target }: ChangeEvent<HTMLInputElement>) => setFile(target.files?.item(0) ?? null),
 		[],
 	);
-	const open = useCallback(() => inputRef.current?.click(), []);
 
 	return {
 		file,
 		onChange,
-		open,
-		ref: inputRef,
 	};
 }
 
 export interface FileInputData {
 	file: File | null;
 	onChange: ChangeEventHandler<HTMLInputElement>;
-	open: () => void;
-	ref: RefObject<HTMLInputElement | null>;
 }
