e0a6ac46a590da54f93a4952680e414ce94a03e4
JJSosaL <hello@fancystudio.xyz>
2026-07-06 23:48:59 +0200

    refactor: fonts

--- apps/vanguard-dashboard/app/root.tsx
+++ apps/vanguard-dashboard/app/root.tsx
@@ -72,7 +72,7 @@ export function Layout({ children }: LayoutProps) {
 				<Meta />
 				<Links />
 			</head>
-			<body className='bg-zinc-950 font-general-sans font-medium text-zinc-50 selection:bg-rose-500 selection:text-rose-50'>
+			<body className='bg-zinc-950 font-medium text-zinc-50 selection:bg-rose-500 selection:text-rose-50'>
 				{children}
 				<ScrollRestoration />
 				<Scripts />
