ddf60833bd7af29b01125c95d3f604cf9b0c22ab
JJSosaL <hello@fancystudio.xyz>
2026-07-06 20:39:24 +0200

    refactor: delete `@vanguard/contracts`

--- packages/contracts/src/shared/Message.ts
+++
@@ -1,8 +0,0 @@
-import { object, string } from 'zod';
-
-const CONTENT_MAXIMUM_LENGTH = 4_000;
-const CONTENT_MINIMUM_LENGTH = 1;
-
-export const MessageSchema = object({
-	content: string().min(CONTENT_MINIMUM_LENGTH).max(CONTENT_MAXIMUM_LENGTH).optional(),
-});
