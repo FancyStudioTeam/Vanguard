a772e2160ec6abae5ed93c276b5aa31c0829a73b
JJSosaL <hello@fancystudio.xyz>
2026-07-22 21:32:30 +0200

    refactor!: from TailwindCSS to UnoCSS

---
+++ apps/vanguard-dashboard-vue/uno.config.ts
@@ -0,0 +1,17 @@
+import presetAttributify from '@unocss/preset-attributify';
+import presetWebFonts from '@unocss/preset-web-fonts';
+import presetWind4 from '@unocss/preset-wind4';
+import { defineConfig } from 'unocss';
+
+export default defineConfig({
+	presets: [
+		presetAttributify(),
+		presetWind4(),
+		presetWebFonts({
+			fonts: {
+				sans: 'General Sans',
+			},
+			provider: 'fontshare',
+		}),
+	],
+});
--- apps/vanguard-dashboard-vue/vite.config.ts
+++ apps/vanguard-dashboard-vue/vite.config.ts
@@ -1,14 +1,14 @@
 import { fileURLToPath, URL } from 'node:url';
 
-import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
+import unoCss from 'unocss/vite';
 import { defineConfig } from 'vite';
 import vueDevTools from 'vite-plugin-vue-devtools';
 
 export default defineConfig({
 	plugins: [
-		tailwindcss(),
+		unoCss(),
 		vue(),
 		vueJsx(),
 		vueDevTools(),
