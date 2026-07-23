7752242624c6778388ccd50fad80bf4ffe4a67f0
JJSosaL <hello@fancystudio.xyz>
2026-07-23 20:31:21 +0200

    refactor: use localization

--- apps/vanguard-dashboard-vue/src/views/Routes/NotFound/NotFoundView.vue
+++ apps/vanguard-dashboard-vue/src/views/Routes/NotFound/NotFoundView.vue
@@ -1,10 +1,16 @@
 <template>
 	<main uno-grid uno-h-dvh uno-place-content-center>
 		<section uno-flex="~ col" uno-gap-4>
-			<h1 id="not-found-text" uno-font-bold uno-text-5xl>404 - Not Found</h1>
+			<h1 uno-font-bold uno-text-5xl>{{ t("not-found.title") }}</h1>
 			<RouterLink uno-button="~ filled size-default" :to="{ name: 'Home' }">
-				Return to Home Page
+				{{ t("not-found.buttons.home") }}
 			</RouterLink>
 		</section>
 	</main>
 </template>
+
+<script lang="ts" setup vapor>
+	import { useI18n } from 'vue-i18n';
+
+	const { t } = useI18n();
+</script>
