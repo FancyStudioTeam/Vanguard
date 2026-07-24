a02ca58d674b04cdc7c379e9ec8ab0647c4fa055
JJSosaL <hello@fancystudio.xyz>
2026-07-24 23:46:57 +0200

    feat: detect user locale

--- apps/vanguard-dashboard/src/main.ts
+++ apps/vanguard-dashboard/src/main.ts
@@ -1,5 +1,6 @@
-import 'virtual:uno.css' with { type: 'css' };
+import 'virtual:uno.css';
 
+import { createHead } from '@unhead/vue/client';
 import { createPinia } from 'pinia';
 import { createApp } from 'vue';
 import { createI18n } from 'vue-i18n';
@@ -10,17 +11,33 @@ import enUS from './locales/en-US.json' with { type: 'json' };
 import esES from './locales/es-ES.json' with { type: 'json' };
 import { router } from './router.ts';
 
+const AVAILABLE_LOCALES = [
+	'en-US',
+	'es-ES',
+] as const;
+
+const locale = getPreferredLocale();
+
 const app = createApp(App);
 
+const head = createHead({
+	init: [
+		{
+			htmlAttrs: {
+				lang: locale,
+			},
+		},
+	],
+});
 const i18n = createI18n<
 	[
 		I18nMessageSchema,
 	],
-	'en-US' | 'es-ES'
+	I18nLocale
 >({
 	fallbackLocale: 'en-US',
 	legacy: false,
-	locale: 'en-US',
+	locale: getPreferredLocale(),
 	messages: {
 		'en-US': enUS,
 		'es-ES': esES,
@@ -28,6 +45,7 @@ const i18n = createI18n<
 });
 const pinia = createPinia();
 
+app.use(head);
 app.use(i18n);
 app.use(pinia);
 app.use(router);
@@ -38,4 +56,27 @@ app.use(DataLoaderPlugin, {
 
 app.mount('#app');
 
+function getPreferredLocale(): I18nLocale {
+	const locale = localStorage.getItem('locale');
+
+	if (isLocale(locale)) {
+		return locale;
+	}
+
+	const preferredLocale = navigator.language;
+
+	if (isLocale(preferredLocale)) {
+		localStorage.setItem('locale', preferredLocale);
+
+		return preferredLocale;
+	}
+
+	return 'en-US';
+}
+
+function isLocale(value: string | null): value is I18nLocale {
+	return value !== null && AVAILABLE_LOCALES.includes(value as I18nLocale);
+}
+
+type I18nLocale = (typeof AVAILABLE_LOCALES)[number];
 type I18nMessageSchema = typeof enUS;
