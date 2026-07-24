76e782fc592793707990c810f559268c116159eb
JJSosaL <hello@fancystudio.xyz>
2026-07-24 23:48:00 +0200

    refactor: delete values

--- apps/vanguard-dashboard/src/components/Layout/Navbar/LeftHand/NavbarLogoComponent.vue
+++ apps/vanguard-dashboard/src/components/Layout/Navbar/LeftHand/NavbarLogoComponent.vue
@@ -1,10 +1,11 @@
 <template>
 	<RouterLink
-		uno-active="scale-95"
+		uno-active-scale-95
 		uno-flex="~ row"
+		uno-focus-outline-none
 		uno-font-bold
 		uno-gap-2
-		uno-hover="opacity-75"
+		uno-hover-opacity-75
 		uno-items-center
 		uno-text-xl
 		uno-transition
