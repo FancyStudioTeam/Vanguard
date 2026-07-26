300becd0db94257d82f677145711acf1898c0e28
JJSosaL <hello@fancystudio.xyz>
2026-07-26 02:35:23 +0200

    feat: add `FooterVanguardTextComponent`

---
+++ apps/vanguard-dashboard/src/components/Layout/Footer/FooterVanguardTextComponent.vue
@@ -0,0 +1,15 @@
+<template>
+	<p
+		style="text-fit: grow;"
+		uno-bg="clip-text linear-to-b"
+		uno-font-bold
+		uno-from-zinc-400
+		uno-px-8
+		uno-select-none
+		uno-text="center transparent"
+		uno-to-transparent
+		uno-uppercase
+	>
+		Vanguard
+	</p>
+</template>
