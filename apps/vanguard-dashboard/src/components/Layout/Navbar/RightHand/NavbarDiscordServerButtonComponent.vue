78bd90d5c62f89c971a992f3ba009a8910ac300a
JJSosaL <hello@fancystudio.xyz>
2026-07-26 02:49:58 +0200

    refactor: change to anchor element

--- apps/vanguard-dashboard/src/components/Layout/Navbar/RightHand/NavbarDiscordServerButtonComponent.vue
+++ apps/vanguard-dashboard/src/components/Layout/Navbar/RightHand/NavbarDiscordServerButtonComponent.vue
@@ -1,14 +1,11 @@
 <template>
-	<button type="button" uno-button="~ outline size-default" @click="handleDiscordServerButton">
+	<a href="https://www.fancystudio.xyz" target="_blank" uno-button="~ outline size-default">
 		{{ t("Layout.Navbar.Buttons.DiscordServer") }}
-	</button>
+	</a>
 </template>
 
 <script lang="ts" setup vapor>
 	import { useI18n } from 'vue-i18n';
 
 	const { t } = useI18n();
-
-	const handleDiscordServerButton = () =>
-		window.open('https://www.fancystudio.xyz/discord', '_blank');
 </script>
