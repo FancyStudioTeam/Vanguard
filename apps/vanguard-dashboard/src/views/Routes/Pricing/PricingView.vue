2b6b23f7af1be2c3a446bf6b5b6ceb5b06eab5ae
JJSosaL <hello@fancystudio.xyz>
2026-08-04 21:42:34 +0200

    feat: add premium title

--- apps/vanguard-dashboard/src/views/Routes/Pricing/PricingView.vue
+++ apps/vanguard-dashboard/src/views/Routes/Pricing/PricingView.vue
@@ -1,20 +1,67 @@
 <template>
-	<main uno-gap-4 uno-max-w-7xl uno-mx-auto uno-p="x-8 y-32" uno-w-full>
+	<main uno-flex="~ col" uno-gap-12 uno-max-w-7xl uno-mx-auto uno-p="x-8 y-32" uno-w-full>
+		<h1
+			id="premium-title"
+			uno-font-bold
+			uno-max-w-5xl
+			uno-mx-auto
+			uno-text="xl balance center md:5xl"
+		>
+			{{ premiumTitle }}
+		</h1>
 		<ComparisonTableComponent />
 	</main>
 </template>
 
 <script lang="ts" setup vapor>
 	import { useSeoMeta } from '@unhead/vue';
-	import { computed } from 'vue';
+	import { animate, splitText, stagger } from 'animejs';
+	import { computed, nextTick, onMounted, watch } from 'vue';
 	import { useI18n } from 'vue-i18n';
 
 	import ComparisonTableComponent from './components/ComparisonTable/ComparisonTableComponent.vue';
 
 	const { t } = useI18n();
 
+	const premiumTitle = computed(() => t('Views.Premium.Title'));
+
 	useSeoMeta({
 		robots: 'follow, index',
 		title: computed(() => t('SEO.Premium.Title')),
 	});
+
+	watch(premiumTitle, async () => {
+		await nextTick();
+
+		animatePremiumTitle();
+	});
+
+	onMounted(animatePremiumTitle);
+
+	function animatePremiumTitle() {
+		const EffectDuration = 550;
+		const EffectStaggerDuration = 150;
+
+		const lines = splitText('#premium-title', {
+			lines: {
+				wrap: 'clip',
+			},
+		});
+
+		lines.addEffect(({ lines }) =>
+			animate(lines, {
+				delay: stagger(EffectStaggerDuration),
+				duration: EffectDuration,
+				ease: 'out(3)',
+				y: [
+					{
+						to: [
+							'100%',
+							'0%',
+						],
+					},
+				],
+			}),
+		);
+	}
 </script>
