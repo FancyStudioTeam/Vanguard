638cf41c603332a422eed1a5b0dfdda6b7111df2
JJSosaL <hello@fancystudio.xyz>
2026-08-04 23:14:02 +0200

    fix: accessibility issues

--- apps/vanguard-dashboard/src/components/Layout/Footer/Vanguard/FooterVanguardSectionComponent.vue
+++ apps/vanguard-dashboard/src/components/Layout/Footer/Vanguard/FooterVanguardSectionComponent.vue
@@ -1,20 +1,22 @@
 <template>
 	<ul uno-flex="~ col" uno-gap-2>
-		<span uno-font-bold uno-text-xs uno-uppercase>Vanguard</span>
-		<ul>
-			<template v-for="link in FOOTER_LINKS" :key="link.locationName">
-				<li>
-					<RouterLink
-						uno-font-semibold
-						uno-text="sm zinc-400 hover:zinc-50"
-						uno-transition
-						:to="{ name: link.locationName }"
-					>
-						{{ t(link.localeKey) }}
-					</RouterLink>
-				</li>
-			</template>
-		</ul>
+		<li uno-font-bold uno-text-xs uno-uppercase>Vanguard</li>
+		<li>
+			<ul>
+				<template v-for="link in FOOTER_LINKS" :key="link.locationName">
+					<li>
+						<RouterLink
+							uno-font-semibold
+							uno-text="sm zinc-400 hover:zinc-50"
+							uno-transition
+							:to="{ name: link.locationName }"
+						>
+							{{ t(link.localeKey) }}
+						</RouterLink>
+					</li>
+				</template>
+			</ul>
+		</li>
 	</ul>
 </template>
 
--- apps/vanguard-dashboard/src/views/Routes/Pricing/PricingView.vue
+++ apps/vanguard-dashboard/src/views/Routes/Pricing/PricingView.vue
@@ -5,7 +5,7 @@
 			uno-font-bold
 			uno-max-w-5xl
 			uno-mx-auto
-			uno-text="xl balance center md:5xl"
+			uno-text="3xl balance center md:5xl"
 		>
 			{{ premiumTitle }}
 		</h1>
@@ -15,7 +15,7 @@
 
 <script lang="ts" setup vapor>
 	import { useSeoMeta } from '@unhead/vue';
-	import { animate, splitText, stagger } from 'animejs';
+	import { animate, splitText, stagger, type TextSplitter } from 'animejs';
 	import { computed, nextTick, onMounted, watch } from 'vue';
 	import { useI18n } from 'vue-i18n';
 
@@ -24,6 +24,8 @@
 	const { t } = useI18n();
 
 	const premiumTitle = computed(() => t('Views.Premium.Title'));
+
+	let lines: TextSplitter | null = null;
 
 	useSeoMeta({
 		robots: 'follow, index',
@@ -39,10 +41,12 @@
 	onMounted(animatePremiumTitle);
 
 	function animatePremiumTitle() {
+		lines?.revert();
+
 		const EffectDuration = 550;
 		const EffectStaggerDuration = 150;
 
-		const lines = splitText('#premium-title', {
+		lines = splitText('#premium-title', {
 			lines: {
 				wrap: 'clip',
 			},
